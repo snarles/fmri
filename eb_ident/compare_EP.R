9da57b7df2d62a111895d50ad417942c4f45cd48
Charles Zheng <snarles@stanford.edu>
2015-05-22 18:35:03 -0700

    eigenprisms!

---
+++ eb_ident/compare_EP.R
@@ -0,0 +1,101 @@
+####
+## COMPARE E BAYES WITH ML
+###
+
+library(magrittr)
+library(pracma)
+library(MASS)
+library(glmnet)
+source('transfer/source.R')
+library(class)
+
+## Generate parameters
+
+n <- 80
+p <- 30
+q <- 60
+
+Sigma_X <- 1/(2 * q) * randn(2 * q, q) %>% { t(.) %*% . }
+Sigma_e <- 10/(2 * p) * randn(2 * p, p) %>% { t(.) %*% . }
+s0s <- rnorm(p)^2
+Sigma_B <- diag(rep(s0s, each = q))
+
+Bvec <- sqrt(Sigma_B) %*% rnorm(p * q)
+B0 <- matrix(Bvec, q, p)
+
+## Generate data
+
+X <- mvrnorm(n, mu = rep(0, q), Sigma = Sigma_X)
+Y <- X %*% B0 + mvrnorm(n, mu = rep(0, p), Sigma = Sigma_e)
+B_mu <- matrix(0, q, p)
+for (i in 1:p) {
+  B_mu[, i] <- solve(t(X) %*% X + diag(rep(1/s0s[i], q))) %*% t(X) %*% Y[, i]    
+}
+Sigma_Bhat_vec <- solve(solve(Sigma_e) %x% (t(X) %*% X) + diag(1/diag(Sigma_B)))
+
+## Estimate parameters
+
+B_mu_EB <- matrix(0, q, p) # placeholder for estimate
+resids <- matrix(0, n, p)
+lambdas <- numeric(p) # lambda for each column of B
+for (i in 1:p) {
+  res <- cv.glmnet(X, Y[, i], alpha = 0, intercept = FALSE, standardize = FALSE)
+  pre <- predict(res, newx = X)
+  resids[, i] <- pre - Y[, i]
+  B_mu_EB[, i] <- as.numeric(coefficients(res))[-1]  
+  lambdas[i] <- (sum(as.numeric(coefficients(res))[-1]^2)/q)  
+}
+lambdas[lambdas < 1e-3] <- 1e-3
+#lambdas <- rep(mean(lambdas), p)
+#lambdas <- s0s
+Sigma_E_hat <- 0.5 * cov(resids) + 0.5 * diag(diag(cov(resids)))
+Inv_Sigma_B <- diag(rep(1/lambdas, each = q))
+EB_cov_B_vec <- solve(solve(Sigma_E_hat) %x% (t(X) %*% X) + Inv_Sigma_B)
+
+# compare estimated sigma_B with true sigma_B
+plot(s0s, lambdas)
+
+## Generate new stimuli
+L <- 200
+n_te <- 200
+X_te <- randn(L, q) 
+i_chosen <- sample(L, n_te, TRUE)
+y_star <- X_te[i_chosen, , drop = FALSE] %*% B0 +  mvrnorm(n_te, rep(0, p), Sigma_e)
+
+## functions
+
+post_probs <- function(Sigma_e, Sigma_vec_B, B_hat, X, Y) {
+  L <- dim(X)[1]
+  n <- dim(Y)[1]
+  p <- dim(Y)[2]
+  mus <- list(L)
+  covs <- list(L)
+  pprobs <- matrix(0, n, L)
+  for (i in 1:L) {
+    mus[[i]] <- X[i, , drop = FALSE] %*% B_hat
+    covs[[i]] <- (diag(rep(1, p)) %x% t(X[i, ])) %*% 
+      Sigma_vec_B %*% (diag(rep(1, p)) %x% t(t(X[i, ]))) + Sigma_e
+    for (j in 1:n) {
+      pprobs[j, i] <- -log(det(covs[[i]])) - 
+        (Y[j, , drop = FALSE] - mus[[i]]) %*% solve(covs[[i]]) %*% 
+        t(Y[j, , drop = FALSE] - mus[[i]])
+    }  
+  }
+  pprobs
+}
+
+## Bayes
+pp_bayes <- post_probs(Sigma_e, Sigma_Bhat_vec, B_mu, X_te, y_star)
+bayes_cl <- apply(pp_bayes, 1, function(v) order(-v)[1])
+(bayes_err <- sum(bayes_cl != i_chosen))
+
+## Maximum likelihood
+mu_hat <- X_te %*% B_mu_EB
+a <- isqrtm(Sigma_E_hat)
+mle_cl <- knn(mu_hat %*% a, y_star %*% a, 1:L)
+(mle_err <- sum(mle_cl != i_chosen))
+
+## E-bayes
+pp_eb <- post_probs(Sigma_E_hat, EB_cov_B_vec, B_mu_EB, X_te, y_star)
+eb_cl <- apply(pp_eb, 1, function(v) order(-v)[1])
+(eb_err <- sum(eb_cl != i_chosen))
