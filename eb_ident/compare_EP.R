02574e8cadcb9cd3029b91901b3edcb3ff875f60
Charles Zheng <snarles@stanford.edu>
2015-05-24 07:08:03 -0700

    bagged ML doesn't work; cleaned up code

--- eb_ident/compare_EP.R
+++ eb_ident/compare_EP.R
@@ -29,50 +29,19 @@ B0 <- matrix(Bvec, q, p)
 
 X <- mvrnorm(n, mu = rep(0, q), Sigma = Sigma_X)
 Y <- X %*% B0 + mvrnorm(n, mu = rep(0, p), Sigma = Sigma_e)
-B_mu <- matrix(0, q, p)
-for (i in 1:p) {
-  B_mu[, i] <- solve(t(X) %*% X + diag(rep(1/s0s[i], q))) %*% t(X) %*% Y[, i]    
-}
-Sigma_Bhat_vec <- solve(solve(Sigma_e) %x% (t(X) %*% X) + diag(1/diag(Sigma_B)))
 
 ## Estimate parameters
 
-# using CV
-B_mu_EB <- matrix(0, q, p) # placeholder for estimate
-resids <- matrix(0, n, p)
-lambdas <- numeric(p) # lambda for each column of B
-for (i in 1:p) {
-  res <- cv.glmnet(X, Y[, i], alpha = 0, intercept = FALSE, standardize = FALSE)
-  pre <- predict(res, newx = X)
-  resids[, i] <- pre - Y[, i]
-  B_mu_EB[, i] <- as.numeric(coefficients(res))[-1]  
-  lambdas[i] <- (sum(as.numeric(coefficients(res))[-1]^2)/q)  
-}
-lambdas <- pmax(lambdas, 1e-5)
-Sigma_E_hat <- 0.5 * cov(resids) + 0.5 * diag(diag(cov(resids)))
-Inv_Sigma_B <- diag(rep(1/lambdas, each = q))
-EB_cov_B_vec <- solve(solve(Sigma_E_hat) %x% (t(X) %*% X) + Inv_Sigma_B)
-
-# Using eigenprism
-res_EP <- eigenprisms(X, Y)
-filt_EP <- (res_EP$T2 > 0)
-lambdas_EP <-  res_EP$T2[filt_EP]/q
-p_EP <- sum(filt_EP)
-B_mu_EP <- matrix(0, q, p_EP)
-Y_EP <- Y[, filt_EP]
-for (i in 1:p_EP) {
-  B_mu_EP[, i] <- solve(t(X) %*% X + diag(rep(lambdas_EP[i], q))) %*% t(X) %*% Y_EP[, i]    
-}
-Yhat_EP <- X %*% B_mu_EP
-Sigma_e_EP <- cov(Y_EP - Yhat_EP) %>% {0.5 * . + 0.5 * diag(diag(.))}
-Sigma_B_EP <- diag(rep(1/lambdas_EP, each = q))
-EP_cov_B_vec <- solve(solve(Sigma_e_EP) %x% (t(X) %*% X) + diag(1/diag(Sigma_B_EP)))
+p_Bayes <- params_Bayes(X, Y, Sigma_e, Sigma_B, s0s)
+p_CV <- params_CV(X, Y, X, Y)
+p_EP <- params_EP(X, Y, X, Y)
+ps_bagged <- mle_bagged(X, Y)
 
 # compare estimated sigma_B with true sigma_B
 #plot(s0s, lambdas)
 plot(colSums(B0^2)/q, s0s)
-points(colSums(B0^2)/q, lambdas, col = "blue")
-points(colSums(B0^2)/q, res_EP$T2/q, col = "red")
+points(colSums(B0^2)/q, p_CV$lambdas, col = "blue")
+points(colSums(B0^2)/q, p_EP$res_EP$T2/q, col = "red")
 abline(0, 1)
 
 ## Generate new stimuli
@@ -81,31 +50,11 @@ n_te <- 200
 X_te <- randn(L, q) 
 i_chosen <- sample(L, n_te, TRUE)
 y_star <- X_te[i_chosen, , drop = FALSE] %*% B0 +  mvrnorm(n_te, rep(0, p), Sigma_e)
-y_star_EP <- y_star[, filt_EP]
 
-## Bayes
-pp_bayes <- post_probs(Sigma_e, Sigma_Bhat_vec, B_mu, X_te, y_star)
-bayes_cl <- apply(pp_bayes, 1, function(v) order(-v)[1])
-(bayes_err <- sum(bayes_cl != i_chosen))
-
-## Maximum likelihood
-mu_hat <- X_te %*% B_mu_EB
-a <- isqrtm(Sigma_E_hat)
-mle_cl <- knn(mu_hat %*% a, y_star %*% a, 1:L)
-(mle_err <- sum(mle_cl != i_chosen))
-
-## Maximum likelihood with EP
-mu_hat <- X_te %*% B_mu_EP
-a <- isqrtm(Sigma_e_EP)
-mle_cl_EP <- knn(mu_hat %*% a, y_star_EP %*% a, 1:L)
-(mle_err_EP <- sum(mle_cl_EP != i_chosen))
-
-## E-bayes
-pp_eb <- post_probs(Sigma_E_hat, EB_cov_B_vec, B_mu_EB, X_te, y_star)
-eb_cl <- apply(pp_eb, 1, function(v) order(-v)[1])
-(eb_err <- sum(eb_cl != i_chosen))
-
-## E-bayes with EP
-pp_ep <- post_probs(Sigma_e_EP, EP_cov_B_vec, B_mu_EP, X_te, y_star_EP)
-ep_cl <- apply(pp_ep, 1, function(v) order(-v)[1])
-(ep_err <- sum(ep_cl != i_chosen))
+## error rates
+(bayes_err <- post_probs(X_te, y_star, i_chosen, p_Bayes)$err)
+(mle_err <- mle_cl(X_te, y_star, i_chosen, p_CV)$err)
+(mle_err_EP <- mle_cl(X_te, y_star, i_chosen, p_EP)$err)
+(eb_err <- post_probs(X_te, y_star, i_chosen, p_CV)$err)
+(ep_err <- post_probs(X_te, y_star, i_chosen, p_EP)$err)
+(bagged_err <- bagged_cl(X_te, y_star, i_chosen, ps_bagged)$err)
--- eb_ident/source.R
+++ eb_ident/source.R
@@ -1,4 +1,20 @@
-post_probs <- function(Sigma_e, Sigma_vec_B, B_hat, X, Y) {
+mle_cl <- function(X, Y0, i_chosen, pars) {
+  Y <- Y0[, pars$filt]
+  L <- dim(X)[1]
+  n <- dim(Y)[1]
+  p <- dim(Y)[2]
+  mu_hat <- X %*% pars$B
+  a <- isqrtm(pars$Sigma_e)
+  mu_hat2 <- mu_hat %*% a
+  Y2 <- Y %*% a
+  pprobs <- (-pdist2(mu_hat2, Y2)) %>% {. - apply(., 2, min)} %>% exp %>% {./rowSums(.)}
+  cl <- apply(pprobs, 1, function(v) order(-v)[1])
+  (mle_err <- sum(cl != i_chosen))
+  list(cl = cl, err = mle_err, pprobs = pprobs)
+}
+
+post_probs <- function(X, Y0, i_chosen, pars) {
+  Y <- Y0[, pars$filt]
   L <- dim(X)[1]
   n <- dim(Y)[1]
   p <- dim(Y)[2]
@@ -6,14 +22,109 @@ post_probs <- function(Sigma_e, Sigma_vec_B, B_hat, X, Y) {
   covs <- list(L)
   pprobs <- matrix(0, n, L)
   for (i in 1:L) {
-    mus[[i]] <- X[i, , drop = FALSE] %*% B_hat
+    mus[[i]] <- X[i, , drop = FALSE] %*% pars$B
     covs[[i]] <- (diag(rep(1, p)) %x% t(X[i, ])) %*% 
-      Sigma_vec_B %*% (diag(rep(1, p)) %x% t(t(X[i, ]))) + Sigma_e
+      pars$Sigma_B %*% (diag(rep(1, p)) %x% t(t(X[i, ]))) + pars$Sigma_e
     for (j in 1:n) {
       pprobs[j, i] <- -log(det(covs[[i]])) - 
         (Y[j, , drop = FALSE] - mus[[i]]) %*% solve(covs[[i]]) %*% 
         t(Y[j, , drop = FALSE] - mus[[i]])
     }  
   }
-  pprobs
+  cl <- apply(pprobs, 1, function(v) order(-v)[1])
+  err <- sum(cl != i_chosen)
+  list(pprobs = pprobs, cl = cl, err = err)
 }
+
+bagged_cl <- function(X, Y, i_chosen, ps) {
+  kk <- length(ps)
+  L <- dim(X)[1]
+  nte <- dim(Y)[1]
+  cl_votes <- matrix(0, nte, L)
+  for (i in 1:kk) {
+    res <- mle_cl(X, Y, i_chosen, ps[[i]])
+    cl_votes %<>% add(res$pprobs)
+  }
+  cl <- apply(cl_votes, 1, function(v) order(-v)[1])
+  err <- sum(cl != i_chosen)
+  list(cl_votes = cl_votes, cl = cl, err = err)
+}
+
+params_CV <- function(X, Y, Xte, Yte) {
+  q <- dim(X)[2]
+  p <- dim(Y)[2]
+  nte <- dim(Yte)[1]
+  B_mu_EB <- matrix(0, q, p) # placeholder for estimate
+  resids <- matrix(0, nte, p)
+  lambdas <- numeric(p) # lambda for each column of B
+  for (i in 1:p) {
+    res <- cv.glmnet(X, Y[, i], alpha = 0, intercept = FALSE, standardize = FALSE,
+                     grouped = FALSE)
+    pre <- predict(res, newx = Xte)
+    resids[, i] <- pre - Yte[, i]
+    B_mu_EB[, i] <- as.numeric(coefficients(res))[-1]  
+    lambdas[i] <- (sum(as.numeric(coefficients(res))[-1]^2)/q)  
+  }
+  lambdas <- pmax(lambdas, 1e-5)
+  Sigma_E_hat <- 0.5 * cov(resids) + 0.5 * diag(diag(cov(resids)))
+  Inv_Sigma_B <- diag(rep(1/lambdas, each = q))
+  EB_cov_B_vec <- solve(solve(Sigma_E_hat) %x% (t(X) %*% X) + Inv_Sigma_B)  
+  list(lambdas = lambdas, B = B_mu_EB, Sigma_e = Sigma_E_hat,  Sigma_B = EB_cov_B_vec,
+       filt = rep(TRUE, p))
+}
+
+params_EP <- function(X, Y, Xte, Yte) {
+  q <- dim(X)[2]
+  p <- dim(Y)[2]
+  nte <- dim(Yte)[1]
+  
+  res_EP <- eigenprisms(X, Y)
+  filt_EP <- (res_EP$T2 > 0)
+  lambdas_EP <-  res_EP$T2[filt_EP]/q
+  p_EP <- sum(filt_EP)
+  B_mu_EP <- matrix(0, q, p_EP)
+  Y_EP <- Y[, filt_EP]
+  for (i in 1:p_EP) {
+    B_mu_EP[, i] <- solve(t(X) %*% X + diag(rep(lambdas_EP[i], q))) %*% t(X) %*% Y_EP[, i]    
+  }
+  Yhat_EP <- X %*% B_mu_EP
+  Sigma_e_EP <- cov(Y_EP - Yhat_EP) %>% {0.5 * . + 0.5 * diag(diag(.))}
+  Sigma_B_EP <- diag(rep(1/lambdas_EP, each = q))
+  EP_cov_B_vec <- solve(solve(Sigma_e_EP) %x% (t(X) %*% X) + diag(1/diag(Sigma_B_EP)))
+  list(lambdas = lambdas_EP, filt = filt_EP, B = B_mu_EP, Sigma_e = Sigma_e_EP,
+       Sigma_B = EP_cov_B_vec, res_EP = res_EP)
+}
+
+params_Bayes <- function(X, Y, Sigma_e, Sigma_B, s0s) {
+  q <- dim(X)[2]
+  p <- dim(Y)[2]
+  B_mu <- matrix(0, q, p)
+  for (i in 1:p) {
+    B_mu[, i] <- solve(t(X) %*% X + diag(rep(1/s0s[i], q))) %*% t(X) %*% Y[, i]    
+  }
+  Sigma_Bhat_vec <- solve(solve(Sigma_e) %x% (t(X) %*% X) + diag(1/diag(Sigma_B)))
+  list(B = B_mu, Sigma_B = Sigma_Bhat_vec, filt = rep(TRUE, p),
+       Sigma_e = Sigma_e)
+}
+
+mle_bagged <- function(X, Y) {
+  p <- dim(X)[2]
+  q <- dim(Y)[2]
+  n <- dim(X)[1]
+  k1 <- 1 + floor((0:4) * n/5)
+  k2 <- floor(1:5 * n / 5)
+  ps <- list(5)
+  sum_e <- matrix(0, q, q)
+  for (i in 1:5) {
+    Xtr <- X[-(k1[i]:k2[i]), ]
+    Ytr <- Y[-(k1[i]:k2[i]), ]
+    Xte <- X[(k1[i]:k2[i]), ]
+    Yte <- Y[(k1[i]:k2[i]), ]
+    ps[[i]] <- params_CV(Xtr, Ytr, Xte, Yte)
+    sum_e <- sum_e + ps[[i]]$Sigma_e/5
+  }
+  for (i in 1:5) ps[[i]]$Sigma_e <- sum_e
+  ps
+}
+
+
