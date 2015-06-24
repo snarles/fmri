edcca283ddd426cb623d9af7425b4d33d5383b52
Charles Zheng <snarles@stanford.edu>
2015-06-24 10:18:34 -0700

    posterior cov via svd

--- bayes_reg.R
+++ bayes_reg.R
@@ -10,29 +10,32 @@ library(MASS)
 f2 <- function(x) sum(x^2)
 
 n <- 20
-pX <- 5 # number of X-features
+nte <- 20
+pX <- 30 # number of X-features
 pY <- 7 # number of Y-responses
 sigma_b <- 0.1 # size of each random coefficent
 # noise covariance
 Sigma_e <- 10 * (1/10/pY) * randn(10 * pY, pY) %>% {t(.) %*% .}
 # Generate X in a way that columns are correlated
-X <- randn(n, 20) %*% abs(randn(20, pX)) + randn(n, pX)
+Xall <- randn(n + nte, 20) %*% abs(randn(20, pX)) + randn(n + nte, pX)
+X <- Xall[1:n, ]
+Xte <- Xall[-(1:n), ]
 # Generate coefficents
 B <- sigma_b * randn(pX, pY)
-E <- mvrnorm(n = n, mu = rep(0, pY), Sigma = Sigma_e)
-Y <- X %*% B + E
+Y <- X %*% B + mvrnorm(n = n, mu = rep(0, pY), Sigma = Sigma_e)
+Yte <- Xte %*% B + mvrnorm(n = nte, mu = rep(0, pY), Sigma = Sigma_e)
+
 
 ## A basic ridge estimate of B (ignores correlations)
 
 Bvec <- as.numeric(B)
-Norm(Bvec)^2
+(err_0 <- Norm(Yte)^2)
 lambda <- mean(diag(Sigma_e))/(sigma_b^2)
-ridge_mu <- as.numeric(solve(t(X) %*% X + lambda * eye(pX), t(X) %*% Y))
-(err_ridge <- Norm(Bvec - ridge_mu)^2)
+ridge_mu <- solve(t(X) %*% X + lambda * eye(pX), t(X) %*% Y)
+ridge_Yte <- Xte %*% ridge_mu
+(err_ridge <- f2(Yte - ridge_Yte))
 
 ## Compute posterior mean and covariance: the naive way
-
-
 
 Omega_e <- solve(Sigma_e)
 IX <- eye(pY) %x% X
@@ -46,18 +49,31 @@ yVec <- as.numeric(Y)
 xtx <- t(X) %*% X
 xt.o.y <- t(IX) %*% Omega_E %*% yVec
 
-#f2(Omega_e %x% xtx - t(IX) %*% Omega_E %*% IX)
-# check precision
-#Norm(Omega_E %*% yVec - as.numeric(t(solve(Sigma_e, t(Y)))))
-
 post_cov <- solve(Omega_e %x% xtx + Omega_B)
 post_mu <- solve(Omega_e %x% xtx + Omega_B, xt.o.y)
-# check precision
-#Norm((Omega_e %x% xtx + Omega_B) %*% post_mu - xt.o.y)
-(err_post <- Norm(post_mu - Bvec)^2)
+Yte_post <- Xte %*% matrix(post_mu, pX, pY)
+(err_post <- f2(Yte - Yte_post))
+
+## Compute posterior using SVD
+
+res <- svd(rbind(X, matrix(0, pX - n, pX)))
+V <- res$v
+D <- diag(res$d)
+f2(xtx - V %*% D^2 %*% t(V))
+
+DU <- diag(res$d) %*% t(res$u)
+IV <- eye(pY) %x% V
+IDU <- eye(pY) %x% DU
+icov_post <- Omega_e %x% xtx + Omega_B
+icov_svd <- IV %*% (Omega_e %x% D^2 + (1/sigma_b^2) * eye(pY * pX)) %*% t(IV)
+#f2(icov_post - icov_svd)
+cov_svd <- IV %*% solve(Omega_e %x% D^2 + (1/sigma_b^2) * eye(pY * pX), t(IV))
+#f2(cov_svd - post_cov)
+mu_svd <- IV %*% solve(Omega_e %x% D^2 + (1/sigma_b^2) * eye(pY * pX), t(IV) %*% t(IX) %*% Omega_E %*% yVec)
+Yte_svd <- Xte %*% matrix(mu_svd, pX, pY)
+(err_svd <- f2(Yte - Yte_svd))
+
+#f2(post_cov_svd - post_cov)
+#max(abs((post_cov_svd - post_cov)))
 
 
-plot(Bvec, ridge_mu)
-
-plot(Bvec, post_mu)
-plot(ridge_mu, post_mu)
