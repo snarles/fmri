14d6ff93e1706bebc11260c8a0c95bc1231e57bb
snarles <snarles@stanford.edu>
2015-09-03 13:54:43 -0700

    new functions

---
+++ lineId/R/bayes.R
@@ -0,0 +1,280 @@
+#' Functions for Bayesian Multivariate Regression
+#' 
+#' @import pracma
+#' @import MASS
+#' @import parallel
+
+#' Computes t(A %x% B) %*% diag(d) %*% (A %*% B)
+#' 
+#' @param A matrix of size a x b
+#' @param B matrix of size c x d
+#' @param d vector: represents diagonal matrix of size a * c
+#' @return square matrix of size a * c
+#' @export
+tkron_d_kron <- function(A, B, d) {
+  a1 <- dim(A)[1]; a2 <- dim(A)[2]; b1 <- dim(B)[1]; b2 <- dim(B)[2]
+  dmat <- matrix(d, b1, a1)
+  # columns of dmat are diag(D1), diag(D2), ...
+  C <- zeros(a2 * b2)
+  if (a1 < b1) {
+    bdbmat <- zeros(b2^2, a1)
+    for (i in 1:a1) bdbmat[, i] <- as.numeric(t(B) %*% (dmat[, i] * B))
+    for (i in 1:a2) {
+      for (j in i:a2) {
+        Cij <- matrix(bdbmat %*% (A[, i] * A[, j]), b2, b2)
+        C[(i-1) * b2 + (1:b2), (j-1) * b2 + (1:b2)] <- Cij
+        C[(j-1) * b2 + (1:b2), (i-1) * b2 + (1:b2)] <- t(Cij)
+      }
+    }
+  } else {
+    for (i in 1:a2) {
+      for (j in i:a2) {
+        dtemp <- as.numeric(dmat %*% (A[, i] * A[, j]))
+        Cij <- t(B) %*% (dtemp * B)
+        C[(i-1) * b2 + (1:b2), (j-1) * b2 + (1:b2)] <- Cij
+        C[(j-1) * b2 + (1:b2), (i-1) * b2 + (1:b2)] <- t(Cij)
+      }
+    }  
+  }
+  C
+}
+
+#' Computes (A %x% B) %*% v
+#' 
+#' @param A matrix of size a x b
+#' @param B matrix of size c x d
+#' @param cc vector of length b * d
+#' @return vector of size a * c
+#' @export
+kron_v <- function(A, B, cc) {
+  a1 <- dim(A)[1]; a2 <- dim(A)[2]; b1 <- dim(B)[1]; b2 <- dim(B)[2]
+  as.numeric(B %*% matrix(cc, b2, a2) %*% t(A))
+}
+
+#' Computes posterior moments for Bayesian multivariate regression
+#' 
+#' Posterior mean and covariance given a prior on Bvec
+#'  of the form Sigma_b %x% eye(pX)
+#'  and noise prior of form Sigma_e %x% Sigma_t
+#' @param X covariate matrix
+#' @param Y response matrix
+#' @param Sigma_e Covariance within given time (row) of Y
+#' @param Sigma_b Prior covariance of a column of B
+#' @param Sigma_t Autocorrelation of a single response (column) of Y over time
+#' @param computeCov Return the posterior covariance of B?
+#' @param naive Do the naive computation (for testing)
+#' @param matrix Return B as a matrix? (or vector)
+#' @return a matrix/vector (if \code{computeCov = FALSE})
+#' or a list (if \code{computeCov = TRUE})
+#' @export
+post_moments <- function(X, Y, Sigma_e, Sigma_b, Sigma_t = eye(dim(X)[1]), 
+                         computeCov = TRUE, naive = FALSE, matrix = TRUE, ...) {
+  n <- dim(X)[1]; pX <- dim(X)[2]; pY <- dim(Y)[2]
+  Omega_e <- solve(Sigma_e)
+  Omega_t <- solve(Sigma_t)
+  Omega_b <- diag(1/diag(Sigma_b))
+  yVec <- as.numeric(Y)
+  xtx <- t(X) %*% Omega_t %*% X
+  if (naive) {
+    ans_mu <- solve(Omega_e %x% xtx + Omega_b %x% eye(pX),
+                    t(eye(pY) %x% X) %*% (Omega_e %x% Omega_t) %*% yVec)
+    if (matrix) ans_mu <- matrix(ans_mu, pX, pY)
+    if (computeCov) {
+      ans_cov <- solve(Omega_e %x% xtx + Omega_b %x% eye(pX))
+      return(list(Mu = ans_mu, Cov = ans_cov))
+    }
+    return(ans_mu)
+  }
+  homega_b <- diag(1/sqrt(diag(Sigma_b)))
+  hsigma_b <- diag(sqrt(diag(Sigma_b)))
+  res <- eigen(hsigma_b %*% Omega_e %*% hsigma_b)
+  D_e <- diag(res$values)
+  V_e <- homega_b %*% res$vectors
+  iV_e <- solve(V_e)
+  resX <- eigen(xtx)
+  V_x <- resX$vectors
+  D_x <- diag(resX$values)
+  d <- 1/(diag(D_e) %x% diag(D_x) + 1)
+  temp <- kron_v(iV_e %*% Omega_e, t(V_x) %*% t(X) %*% Omega_t, yVec)
+  ans_mu <- kron_v(t(iV_e), V_x, d * temp)
+  if (matrix) ans_mu <- matrix(ans_mu, pX, pY)
+  if (computeCov) {
+    ans_cov <- tkron_d_kron(iV_e, t(V_x), d)
+    return(list(Mu = ans_mu, Cov = ans_cov))
+  }
+  ans_mu
+}
+
+
+#' Posterior predictive means
+#' 
+#' Returns a list with entry for every row of Xte, which is a list with mu, cov
+#' @param X covariate matrix
+#' @param Y response matrix
+#' @param X_te covariates for responses to be predicted
+#' @param Sigma_e Covariance within given time (row) of Y
+#' @param Sigma_b Prior covariance of a column of B
+#' @param Sigma_t Autocorrelation of a single response (column) of Y over time
+#' @param naive Do the naive computation (for testing)
+#' @param mc.cores How many cores to use in parallel (0 for single-core)
+#' @return a list of list, one sublist per row of X_te,
+#' containing posterior mean and covariance
+#' @export
+post_predictive <- function(X, Y, X_te, Sigma_e, Sigma_b, Sigma_t = eye(dim(X)[1]), 
+                            naive = FALSE, mc.cores = 0, ...) {
+  n <- dim(X)[1]; pX <- dim(X)[2]; pY <- dim(Y)[2]
+  L <- dim(X_te)[1]; ans <- as.list(numeric(L))
+  Omega_e <- solve(Sigma_e)
+  Omega_t <- solve(Sigma_t)
+  Omega_b <- diag(1/diag(Sigma_b))
+  yVec <- as.numeric(Y)
+  xtx <- t(X) %*% Omega_t %*% X
+  
+  if (naive) {
+    res <- post_moments(X, Y, Sigma_e, Sigma_b, Sigma_t, naive = TRUE, matrix = TRUE)
+    B <- res$Mu
+    SigmaB <- res$Cov
+    for (i in 1:L) {
+      x_star <- X_te[i, ]
+      Ix <- eye(pY) %x% t(x_star)
+      Mu <- as.numeric(t(x_star) %*% B)
+      Cov <- Ix %*% SigmaB %*% t(Ix) + Sigma_e
+      ans[[i]] <- list(Mu = Mu, Cov = Cov)
+    }
+  } else {
+    homega_b <- diag(1/sqrt(diag(Sigma_b)))
+    hsigma_b <- diag(sqrt(diag(Sigma_b)))
+    res <- eigen(hsigma_b %*% Omega_e %*% hsigma_b)
+    D_e <- diag(res$values)
+    V_e <- homega_b %*% res$vectors
+    iV_e <- solve(V_e)
+    resX <- eigen(xtx)
+    V_x <- resX$vectors
+    D_x <- diag(resX$values)
+    d <- 1/(diag(D_e) %x% diag(D_x) + 1)
+    temp <- d * kron_v(iV_e %*% Omega_e, t(V_x) %*% t(X) %*% Omega_t, yVec)
+    pre_moments <- function(i) {
+      x_star <- X_te[i, ]
+      Mu <- kron_v(t(iV_e), t(x_star) %*% V_x, temp)
+      Cov <- tkron_d_kron(iV_e, t(V_x) %*% x_star, d) + Sigma_e
+      list(Mu = Mu, Cov = Cov)
+    }
+    if (mc.cores == 0) {
+      ans <- lapply(1:L, pre_moments)
+    } else {
+      ans <- mclapply(1:L, pre_moments, mc.cores = mc.cores)
+    }
+  }
+  ans
+}
+
+
+
+#' Ridge regression subroutine
+#' 
+#' computes solve(xtx + lambda, t(X) %*% Y)
+#' as 1/lambda * t(X) %*% Y
+#' V_X, d_X are from svd of X
+#' @param X covariate/design matrix
+#' @param V_X from svd of X
+#' @param d_X from svd of X
+#' @param lambda ridge regularization
+#' @param Y response matrix
+#' @param naive Do the naive computation (for testing)
+#' @return a matrix B
+#' @export
+fast_ridge <- function(X, V_X, d_X, lambda, Y, naive = FALSE) {
+  if (naive) {
+    return(solve(t(X) %*% X + lambda * eye(dim(X)[2]), t(X) %*% Y))
+  }
+  if (!is.null(X)) {
+    xty <- t(X) %*% Y    
+  } else {
+    xty <- Y
+  }
+  temp1 <- 1/lambda * xty
+  d2 <- (1/(d_X^2 + lambda)) - 1/lambda
+  temp2 <- V_X %*% (d2 * (t(V_X) %*% xty))
+  temp1 + temp2
+}
+
+#' Sampling distribution of ridge regression
+#'
+#' @param X covariate matrix
+#' @param Y response matrix
+#' @param Sigma_e Covariance within given time (row) of Y
+#' @param lambdas Ridge regularization per column of B
+#' @param Sigma_t Autocorrelation of a single response (column) of Y over time
+#' @param computeCov Return the posterior covariance of B?
+#' @param matrix Return B as a matrix? (or vector)
+#' @return a matrix/vector (if \code{computeCov = FALSE})
+#' or a list (if \code{computeCov = TRUE})
+#' @export
+samp_moments <- function(X, Y, Sigma_e, lambdas, Sigma_t = eye(dim(X)[1]), 
+                         computeCov = TRUE, matrix = TRUE, ...) {
+  n <- dim(X)[1]; pX <- dim(X)[2]; pY <- dim(Y)[2]
+  res_X <- svd(X); V_X <- res_X$v; d_X <- res_X$d
+  xtx <- t(X) %*% X
+  xtsx <- t(X) %*% Sigma_t %*% X
+  #temp <- lapply(1:pY, function(i) solve(xtx + lambdas[i] * eye(pX), t(X) %*% Y[, i]))
+  temp <- lapply(1:pY, function(i) fast_ridge(X, V_X, d_X, lambdas[i], Y[, i]))
+  if (matrix) {
+    ans_mu <- do.call(cbind, temp)
+  } else {
+    ans_mu <- do.call(c, temp)
+  }
+  if (computeCov) {
+    ans_cov <- zeros(pX * pY, pX * pY)
+    #d1s <- lapply(1:pY, function(i) solve(xtx + lambdas[i] * eye(pX), xtsx))
+    #d2s <- lapply(1:pY, function(i) solve(xtx + lambdas[i] * eye(pX)))
+    for (i in 1:pY) {
+      for (j in i:pY) {
+        #temp <- Sigma_e[i, j] * d1s[[i]] %*% d2s[[j]]
+        temp0 <- fast_ridge(X, V_X, d_X, lambdas[j], Sigma_t)
+        temp <- Sigma_e[i, j] * fast_ridge(X, V_X, d_X, lambdas[i], t(temp0))
+        ans_cov[(i-1)*pX+(1:pX),(j-1)*pX+(1:pX)] <- temp
+        ans_cov[(j-1)*pX+(1:pX),(i-1)*pX+(1:pX)] <- t(temp)
+      }
+    }
+    return(list(Mu = ans_mu, Cov = ans_cov))
+  }
+  ans_mu
+}
+
+#' Frequentist predictive distributions for ridge regression
+#' 
+#' Returns a list with entry for every row of Xte, which is a list with mu, cov
+#' @param X covariate matrix
+#' @param Y response matrix
+#' @param X_te covariates for responses to be predicted
+#' @param Sigma_e Covariance within given time (row) of Y
+#' @param lambdas Ridge regularization per column of B
+#' @param Sigma_t Autocorrelation of a single response (column) of Y over time
+#' @param mc.cores How many cores to use in parallel (0 for single-core)
+#' @return a list of list, one sublist per row of X_te,
+#' containing posterior mean and covariance
+#' @export
+samp_predictive <- function(X, Y, X_te, Sigma_e, lambdas, Sigma_t = eye(dim(X)[1]), 
+                            mc.cores = 0, ...) {
+  n <- dim(X)[1]; pX <- dim(X)[2]; pY <- dim(Y)[2]
+  L <- dim(X_te)[1]; ans <- as.list(numeric(L))
+  res <- samp_moments(X, Y, Sigma_e, lambdas, Sigma_t, computeCov = TRUE, matrix = TRUE)
+  BMu <- res$Mu
+  BCov <- res$Cov
+  pre_moments <- function(i) {
+    x_star <- X_te[i, ]
+    Mu <- as.numeric(t(x_star) %*% BMu)
+    temp <- zeros(pY, pY * pX)
+    for (ii in 1:pY) {
+      temp[ii, ] <- t(x_star) %*% BCov[(ii - 1)*pX + (1:pX), ]
+    }
+    Cov <- temp %*% (eye(pY) %x% t(t(x_star))) + Sigma_e
+    list(Mu = Mu, Cov = Cov)
+  }
+  if (mc.cores == 0) {
+    ans <- lapply(1:L, pre_moments)
+  } else {
+    ans <- mclapply(1:L, pre_moments, mc.cores = mc.cores)
+  }
+  ans
+}
---
+++ lineId/R/linAlg.R
@@ -0,0 +1,34 @@
+#' Inverse square root of a matrix
+#' 
+#' @param m matrix
+#' @export
+isqrtm <- function(m) {
+  res <- eigen(m)
+  d <- res$values
+  if (min(d) < -1e-5) warning("Negative eigenvalues in isqrtm")
+  d[d < 0] <- 0
+  d[d > 0] <- 1/sqrt(d[d > 0])
+  v <- res$vectors
+  return (v %*% diag(d) %*% t(v))
+}
+
+#' Square root of a matrix
+#' 
+#' @param m matrix
+#' @export
+sqrtm <- function(m) {
+  res <- eigen(m)
+  d <- res$values
+  if (min(d) < -1e-5) warning("Negative eigenvalues in sqrtm")
+  d[d < 0] <- 0
+  d[d > 0] <- sqrt(d[d > 0])
+  v <- res$vectors
+  return (v %*% diag(d) %*% t(v))
+}
+
+#' Frobenius norm squared
+#' 
+#' @param x
+#' @param y (optional)
+f2 <- function(x, y = 0) sum((x - y)^2)
+
