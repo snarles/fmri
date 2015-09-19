397bcb5ebd7579eba6d3e1d97152ee6a7a9ed4e6
snarles <snarles@stanford.edu>
2015-09-18 20:04:24 -0700

    small k, small tr, large d -> universality, large k, large tr, small d -> heterogeneity

--- approximation/gaussian_lc_highdim.R
+++ approximation/gaussian_lc_highdim.R
@@ -47,3 +47,59 @@ par(bg = "grey")
 matplot(mat, type = "l", col = rainbow(length(Sigmas)), lty = 1, lwd = 1)
 lines(1:maxK, mcK_I(cc, maxK), lwd = 2)
 
+
+####
+##  Increase C and number of classes
+####
+
+
+## C = 2 is fine w/ > 100 dimensions
+cc <- 2
+plot(mcK_I(cc, 50), type = "l")
+maxK <- 50
+scalings <- floor((1:20)^1.5)
+Sigmas <- lapply(scalings, function(s) SigHd(cc, s))
+res <- mclapply(Sigmas, function(s) mcK(s, maxK, N = 2000, mc.reps=10), mc.cores = mcc)
+mat <- do.call(cbind, res)
+par(bg = "grey")
+matplot(mat, type = "l", col = rainbow(length(Sigmas)), lty = 1, lwd = 1)
+lines(1:maxK, mcK_I(cc, maxK), lwd = 2)
+
+## C = 3 shows a small gap
+cc <- 3.5
+plot(mcK_I(cc, 70), type = "l")
+maxK <- 70
+scalings <- floor((1:20)^1.5)
+Sigmas <- lapply(scalings, function(s) SigHd(cc, s))
+res <- mclapply(Sigmas, function(s) mcK(s, maxK, N = 2000, mc.reps=10), mc.cores = mcc)
+mat <- do.call(cbind, res)
+par(bg = "grey")
+matplot(mat, type = "l", col = rainbow(length(Sigmas)), lty = 1, lwd = 1)
+lines(1:maxK, mcK_I(cc, maxK), lwd = 2)
+
+
+## C = 5 shows a gap at 100 dims
+cc <- 5
+plot(mcK_I(cc, 200), type = "l")
+maxK <- 200
+scalings <- floor((1:20)^1.5)
+Sigmas <- lapply(scalings, function(s) SigHd(cc, s))
+res <- mclapply(Sigmas, function(s) mcK(s, maxK, N = 2000, mc.reps=10), mc.cores = mcc)
+mat <- do.call(cbind, res)
+par(bg = "grey")
+matplot(mat, type = "l", col = rainbow(length(Sigmas)), lty = 1, lwd = 1)
+lines(1:maxK, mcK_I(cc, maxK), lwd = 2)
+
+## C = 10 already a massive gap
+cc <- 10
+plot(mcK_I(cc, 1000), type = "l")
+maxK <- 1000
+scalings <- floor((1:20)^1.5)
+Sigmas <- lapply(scalings, function(s) SigHd(cc, s))
+res <- mclapply(Sigmas, function(s) mcK(s, maxK, N = 2000, mc.reps=10), mc.cores = mcc)
+mat <- do.call(cbind, res)
+par(bg = "grey")
+matplot(mat, type = "l", col = rainbow(length(Sigmas)), lty = 1, lwd = 1)
+lines(1:maxK, mcK_I(cc, maxK), lwd = 2)
+
+
