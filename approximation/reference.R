80a8972730ba75776d303cd3eef9ca9287282c6a
Charles Zheng <snarles@stanford.edu>
2015-10-18 10:41:54 -0700

    distribution of dot product (standard formula)

---
+++ approximation/reference.R
@@ -0,0 +1,20 @@
+####
+##  Checking standard formulae
+####
+
+## Distribution of dot product
+##   if t = <x, y> for x, y in S^{d-1}
+##   then (t + 1)/2 ~ Beta((d-1)/2, (d-1)/2)
+
+get_dot <- function(d) {
+  x <- rnorm(d)
+  x <- x/sqrt(sum(x^2))
+  x[1]
+}
+
+mc.reps <- 1e4
+d <- 10
+ips <- sapply(1:mc.reps, function(i) get_dot(d))
+plot(1:mc.reps/mc.reps, sort(pbeta((ips + 1)/2, (d-1)/2, (d-1)/2)), type = 'l')
+abline(0, 1, col = "red")
+
