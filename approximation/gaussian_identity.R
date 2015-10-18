614b8df3716f63fa86b5116a7c5385775df570c5
snarles <snarles@stanford.edu>
2015-10-17 21:19:40 -0700

    does dimensionality effect happen at high dimensions?  need to resolve numerical issues in pchisq for high x, df and ncp

--- approximation/gaussian_identity.R
+++ approximation/gaussian_identity.R
@@ -4,7 +4,7 @@ library(class)
 library(parallel)
 library(reginference) ## see github.com/snarles/misc
 library(lineId) ## use devtools::install('lineId')
-source("approximation/extreme_value.R")
+#source("approximation/extreme_value.R")
 
 ## mu ~ N(0, I)
 ## y ~ N(mu*, sigma^2 I)
@@ -22,6 +22,7 @@ mc_ident <- function(p, sigma2, K, mc.reps = 1000) {
 }
 
 ## uses noncentral chi squared
+## same Evalue as mc_ident
 mc_ident2 <- function(p, sigma2, K, mc.reps = 1000) {
   alpha <- sigma2/(1 + sigma2)
   mcs <- sapply(1:mc.reps,
@@ -60,69 +61,97 @@ mc_ident3 <- function(p, sigma2, K, mc.reps = 1000) {
   mean(mcs)
 }
 
-min_chisq_approx <- function(K, df, ncp, naive = FALSE) {
+min_chisq_approx <- function(K, df, ncp, naive = FALSE, nits = 20) {
   if (naive) {
     print(min(rchisq(K, df, ncp)))
   }
-  z <- (ncp - df)/sqrt(df)
-  med_loc2(df, z, K)
+  #z <- (ncp - df)/sqrt(df)
+  #med_loc2(df, z, K, ux=df + ncp, nits=nits)
+  ux <- df + ncp; lx <- 0
+  const <- -log(2)
+  for (i in 1:nits) {
+    xc <- (ux + lx)/2
+    val <- K * log_1_plus(-pchisq(xc, df, ncp))
+    if (val > const) {
+      lx <- xc
+    } else {
+      ux <- xc
+    }
+    #print(xc)
+  }
+  xc
 }
 
 ## computes theoretical median of min
-mc_ident4 <- function(p, sigma2, K, mc.reps = 1000) {
+mc_ident4 <- function(p, sigma2, K, mc.reps = 1000, nits = 20) {
   alpha <- sigma2/(1 + sigma2)
   mcs <- sapply(1:mc.reps,
                 function(i) {
                   y2 <- (1 + sigma2) * rchisq(1, df = p)
                   d1 <- alpha * rchisq(1, df = p, ncp = alpha * y2)
-                  ds <- min_chisq_approx(K - 1, p, y2)
+                  ds <- min_chisq_approx(K - 1, p, y2, nits = nits)
                   (ds < d1)
                 })
   mean(mcs)
 }
 
-
-mean(rchisq(1e3, 3, 2.2))
-mean(rchisq_g(1e3, 3, 2.2))
-var(rchisq(1e3, 3, 2.2))
-var(rchisq_g(1e3, 3, 2.2))
-
-## Gaussian approximation is unusably bad
-
-mc_ident(10, 3, 10, 1e4)
-mc_ident2(10, 3, 10, 1e4)
-mc_ident4(10, 3, 10, 1e4)
-
-mc_ident( 20, 1, 1e3, 1e3)
-mc_ident2(20, 1, 1e3, 1e3)
-mc_ident4(20, 1, 1e3, 1e3)
+## nonrandom -- computes theoretical median of min and uses cdf
+## same Evalue as mc_ident4
+mc_ident5 <- function(p, sigma2, K, mc.reps = 1000, nits = 20) {
+  alpha <- sigma2/(1 + sigma2)
+  quants <- (1:mc.reps - .5)/mc.reps
+  y2s <- (1 + sigma2) * qchisq(quants, df = p)
+  mcs <- sapply(y2s,
+                function(y2) {
+                  ds <- min_chisq_approx(K - 1, p, y2, nits = nits)
+                  1 - pchisq(ds/alpha, p, alpha * y2) 
+                })
+  mean(mcs)
+}
 
 
-df <- 40; sigma2 <- 2; L <- 1e5; nits <- 100
 
-tims <- list(); res <- numeric()
-t1 <- proc.time()
-(res[1] <- mc_ident( df, sigma2, L, 1))
-(tims[[1]] <- proc.time() - t1)
-
-t1 <- proc.time()
-(res[2] <- mc_ident2(df, sigma2, L, nits))
-(tims[[2]] <- proc.time() - t1)
-
-t1 <- proc.time()
-(res[3] <- mc_ident4(df, sigma2, L, nits))
-(tims[[3]] <- proc.time() - t1)
-
-res
-tims
-
-
-## push the dimensionality and # of classes
-
-min_chisq_approx(1e5, 20, 20, TRUE)
-min_chisq_approx(1e6, 30, 30, TRUE)
-t1 <- proc.time()
-min_chisq_approx(1e8, 40, 40, TRUE); gc()
-proc.time() - t1
+# mean(rchisq(1e3, 3, 2.2))
+# mean(rchisq_g(1e3, 3, 2.2))
+# var(rchisq(1e3, 3, 2.2))
+# var(rchisq_g(1e3, 3, 2.2))
+# 
+# ## Gaussian approximation is unusably bad
+# 
+# mc_ident(10, 3, 10, 1e4)
+# mc_ident2(10, 3, 10, 1e4)
+# mc_ident4(10, 3, 10, 1e4)
+# 
+# mc_ident( 20, 1, 1e3, 1e3)
+# mc_ident2(20, 1, 1e3, 1e3)
+# mc_ident4(20, 1, 1e3, 1e3)
+# 
+# 
+# df <- 40; sigma2 <- 2; L <- 1e5; nits <- 100
+# 
+# tims <- list(); res <- numeric()
+# t1 <- proc.time()
+# (res[1] <- mc_ident( df, sigma2, L, 1))
+# (tims[[1]] <- proc.time() - t1)
+# 
+# t1 <- proc.time()
+# (res[2] <- mc_ident2(df, sigma2, L, nits))
+# (tims[[2]] <- proc.time() - t1)
+# 
+# t1 <- proc.time()
+# (res[3] <- mc_ident4(df, sigma2, L, nits))
+# (tims[[3]] <- proc.time() - t1)
+# 
+# res
+# tims
+# 
+# 
+# ## push the dimensionality and # of classes
+# 
+# min_chisq_approx(1e5, 20, 20, TRUE)
+# min_chisq_approx(1e6, 30, 30, TRUE)
+# t1 <- proc.time()
+# min_chisq_approx(1e8, 40, 40, TRUE); gc()
+# proc.time() - t1
 
 
---
+++ approximation/gaussian_identity_highdim.R
@@ -0,0 +1,106 @@
+source("approximation/gaussian_identity.R")
+
+#### 
+##  Check fixed-L results
+####
+
+noise_const <- 2
+L <- 5
+p <- 10; mc_ident(p, noise_const * p, L, 1e4)
+p <- 20; mc_ident(p, noise_const * p, L, 1e4)
+p <- 30; mc_ident(p, noise_const * p, L, 1e4)
+
+p <- 10; mc_ident2(p, noise_const * p, L, 1e4)
+p <- 20; mc_ident2(p, noise_const * p, L, 1e4)
+p <- 30; mc_ident2(p, noise_const * p, L, 1e4)
+
+p <- 10; mc_ident4(p, noise_const * p, L, 1e4)
+p <- 10; mc_ident5(p, noise_const * p, L, 1e2)
+
+
+noise_const <- .2
+L <- 1e3
+p <- 1e3; mc_ident2(p, noise_const * p, L, 1e4)
+mc_ident4(p, noise_const * p, L, 1e2)
+mc_ident5(p, noise_const * p, L, 1e2)
+
+p <- 2e3; mc_ident2(p, noise_const * p, L, 1e4)
+mc_ident4(p, noise_const * p, L, 1e2)
+mc_ident5(p, noise_const * p, L, 1e2)
+
+p <- 3e3; mc_ident2(p, noise_const * p, L, 1e4)
+mc_ident4(p, noise_const * p, L, 1)
+mc_ident5(p, noise_const * p, L, 1)
+
+
+
+
+
+noise_const <- .05
+L <- 1e4
+p <- 1e3; mc_ident2(p, noise_const * p, L, 1e3)
+mc_ident4(p, noise_const * p, L, 1e3)
+mc_ident5(p, noise_const * p, L, 1e3)
+
+p <- 2e3; mc_ident2(p, noise_const * p, L, 1e4)
+mc_ident4(p, noise_const * p, L, 1e2)
+mc_ident5(p, noise_const * p, L, 1e2)
+
+p <- 3e3; mc_ident2(p, noise_const * p, L, 1e4)
+mc_ident4(p, noise_const * p, L, 10)
+mc_ident5(p, noise_const * p, L, 1e2)
+
+
+# sigma2 <- noise_const * p
+# y2 <- (1 + sigma2) * rchisq(1, df = p)
+# K <- L; df <- p; ncp <- y2; nits <- 20
+
+noise_const <- .0525
+L <- 1e5; mc.reps <- 1e2
+p <- 1e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 2e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 4e3; mc_ident5(p, noise_const * p, L, mc.reps)
+
+noise_const <- .0425
+L <- 1e6; mc.reps <- 1e2
+p <- 1e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 2e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 4e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 8e3; mc_ident5(p, noise_const * p, L, mc.reps)
+
+noise_const <- .0355
+L <- 1e7; mc.reps <- 1e2
+p <- 1e3; mc_ident5(p, noise_const * p, L, mc.reps)
+p <- 2e3; mc_ident5(p, noise_const * p, L, mc.reps)
+
+
+
+# Dbugging
+sigma2 <- noise_const * p
+K <- L; df <- p; nits <- 20
+mc.reps <- 5
+quants <- (1:mc.reps - .5)/mc.reps
+y2s <- (1 + sigma2) * qchisq(quants, df = p)
+## dbugging inner
+K <- L-1
+ncp <- y2s[1]
+ux <- df + ncp; lx <- 0
+const <- -log(2)
+for (i in 1:nits) {
+  (xc <- (ux + lx)/2)
+  pchisq(xc, df, ncp)
+  (val <- K * log_1_plus(-pchisq(xc, df, ncp)))
+  if (val > const) {
+    lx <- xc
+  } else {
+    ux <- xc
+  }
+  #print(xc)
+}
+
+## Error~~!
+df <- 8000; ncp <- 2672870
+pchisq(df + ncp, df, ncp)
+pchisq(2010653, 8000, 2672870)
+
+
