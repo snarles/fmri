1d2d9bcfbe8c8ce5d02049b2a216561bf1f187b2
Charles Zheng <snarles@stanford.edu>
2015-10-18 11:49:49 -0700

    large (actually, small) deviations of non-central chi2

--- approximation/reference.R
+++ approximation/reference.R
@@ -2,9 +2,11 @@
 ##  Checking standard formulae
 ####
 
+####
 ## Distribution of dot product
 ##   if t = <x, y> for x, y in S^{d-1}
 ##   then (t + 1)/2 ~ Beta((d-1)/2, (d-1)/2)
+####
 
 get_dot <- function(d) {
   x <- rnorm(d)
@@ -18,3 +20,12 @@ ips <- sapply(1:mc.reps, function(i) get_dot(d))
 plot(1:mc.reps/mc.reps, sort(pbeta((ips + 1)/2, (d-1)/2, (d-1)/2)), type = 'l')
 abline(0, 1, col = "red")
 
+####
+## Chi distribution
+####
+
+library(Runuran)
+dchi <- function(x, df) 2^(1 - (df/2)) * x^(df - 1) * exp(-x^2/2)/gamma(df/2)
+x <- 20; df <- 10
+ud(udchi(df), x)
+dchi(x, df)
