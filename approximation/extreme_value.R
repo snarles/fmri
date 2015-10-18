646273336559ffd8a8c1fd62ebd14b43f62704d3
Charles Zheng <snarles@stanford.edu>
2015-10-18 09:42:35 -0700

    fixed sigm2, logL linear

--- approximation/extreme_value.R
+++ approximation/extreme_value.R
@@ -9,13 +9,13 @@ log_1_plus <- function(x) {
 }
 
 ## med_loc breaks down at k > 20
-med_loc <- function(k, z, L) qchisq(1 - (.5)^(1/L), k, k + z * sqrt(k))
+med_loc <- function(k, ncp, L) qchisq(1 - (.5)^(1/L), k, ncp)
 
-med_loc2 <- function(k, z, L, lx = 0, ux = 100, nits = 100) {
+med_loc2 <- function(k, ncp, L, lx = 0, ux = 100, nits = 100) {
   const <- -log(2)
   for (i in 1:nits) {
     xc <- (ux + lx)/2
-    val <- L * log_1_plus(-pchisq(xc, k, k + z * sqrt(k)))
+    val <- L * log_1_plus(-pchisq(xc, k, ncp))
     if (val > const) {
       lx <- xc
     } else {
@@ -27,9 +27,21 @@ med_loc2 <- function(k, z, L, lx = 0, ux = 100, nits = 100) {
 }
 
 
-get_L <- function(x, k, z) {
-  pp <- pchisq(x, k, k + z * sqrt(k))
+get_L <- function(x, k, ncp) {
+  pp <- pchisq(x, k, ncp)
   log(1/2)/log_1_plus(-pp)
+}
+
+#x <- 0.1; k <- 1; ncp <- 1.1
+#x=60;k=600;ncp=660
+get_log_L <- function(x, k, ncp) {
+  pp <- pchisq(x, k, ncp, log.p = TRUE)
+  if (pp > -10) {
+    pp <- log(-log_1_plus(-exp(pp)))
+  }
+  ans <- log(log(2)) - pp
+  if (ans == Inf) cat(paste0("x=", x, ";k=", k, ";ncp=", ncp))
+  ans
 }
 
 mincdf <- function(x, k, z, L) 1 - exp(L * log_1_plus(-pchisq(x, k, k + z * sqrt(k))))
@@ -78,3 +90,23 @@ minpdf <- function(x, k, z, L) {
 #   Xs[, k] <- sapply(10:100, function(i) med_loc2(i, lalas[k], Ls[i, x_med]))  
 # }
 # matplot(10:100, Xs, type = "l")
+
+####
+##  Fix sigma2, increase d, fix error prob
+####
+
+fixed_error_L <- function(d, sigma2) {
+  ey2 <- (1 + sigma2) * d
+  alpha <- sigma2/(1 + sigma2)
+  ed1 <- alpha * (alpha * ey2 + d)
+  get_log_L(ed1, d, ey2)
+}
+
+sigma2 <- 3
+ds <- 1:1000
+logLs <- sapply(ds, function(d) fixed_error_L(d, sigma2))
+plot(logLs, type = "l")
+
+
+
+
