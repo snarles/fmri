c82b853790ba573837c6adebff110e488c10a1ba
snarles <snarles@stanford.edu>
2015-10-21 13:54:15 -0700

    got ridge regression error formula

--- decision_theory/gcv.R
+++ decision_theory/gcv.R
@@ -19,21 +19,33 @@ gcv_objective_ <- function(X, y) {
 # n <- 30; p <- 200;
 # X <- randn(n, p)
 # bt <- rnorm(p)
-# y <- as.numeric(X %*% bt + rnorm(n))
+# err <- rnorm(n)
+# y <- as.numeric(X %*% bt + err)
 # res <- svd(X)
-# U <- res$u
-# d <- res$d
+# U <- res$u; V <- res$v; d <- res$d
+# dim(V)
 # xi <- as.numeric(t(U) %*% y)
 # 
 # ## verify some formulas
 # lambda <- 2
 # yhat <- X %*% solve(t(X) %*% X + lambda * eye(p), t(X) %*% y)
 # yhat2 <- U %*% diag(d^2/(d^2 + lambda)) %*% t(U) %*% y
+# bhat <-solve(t(X) %*% X + lambda * eye(p), t(X) %*% y)
+# bhat2 <- bt - lambda * solve(t(X) %*% X + lambda * eye(p), bt) + 
+#   solve(t(X) %*% X + lambda * eye(p), t(X) %*% err)
+# bias <- bt - solve(t(X) %*% X + lambda * eye(p), t(X) %*% X %*% bt)
+# bias2 <- (V %*% diag(lambda/(d^2 + lambda) - 1) %*% t(V) + eye(p)) %*% bt
+# f2(bias, bias2)
+# f2(bhat, bhat2)
 # (trr <- sum(diag(X %*% solve(t(X) %*% X + lambda * eye(p), t(X)))))
 # sum(d^2/(d^2 + lambda))
 # f2(y - yhat)
 # f2(y) + sum(xi^2 * ((d^2/(d^2 + lambda))^2 - 2 * (d^2/(d^2 + lambda))))
 # 
 # f2(yhat, yhat2)
+# sqmat <- solve(t(X) %*% X + lambda * eye(p)) %*% 
+#   solve(t(X) %*% X + lambda * eye(p))
+# sqmat2 <- 1/(lambda^2) * (eye(p) + V %*% diag((lambda/(d^2 + lambda))^2 - 1) %*% t(V))
+# f2(sqmat, sqmat2)
 
 
---
+++ decision_theory/ridge_error.R
@@ -0,0 +1,37 @@
+library(MASS)
+library(pracma)
+library(parallel)
+
+f2 <- function(x, y=0) sum((x-y)^2)
+
+ridge_error_trial <- function(bt, X, lambda) {
+  #p <- dim(Sigma)[1]
+  #X <- mvrnorm(n, mu = rep(0, p), Sigma = Sigma)
+  y <- X %*% bt + rnorm(dim(X)[1])
+  btr <- solve(t(X) %*% X + lambda * eye(dim(X)[2]), t(X) %*% y)
+  f2(bt, btr)
+}
+
+ridge_error_trials <- function(bt, X, lambda,
+                               mc.reps = 100, mc.cores = 7) {
+  res <- mclapply(1:mc.reps, function(i) {
+    set.seed(i)
+    ridge_error_trial(bt, X, lambda)
+  }, mc.cores = mc.cores)
+  mean(unlist(res))
+}
+
+
+ridge_error_theory <- function(bt, X, lambda) {
+  res <- svd(X); V <- res$v; d <- res$d; p <- dim(X)[2]
+  vbt <- t(V) %*% bt
+  diags <- (lambda/(d^2 + lambda)) - 1  
+  diags2 <- (lambda/(d^2 + lambda))^2 - 1
+  bias_term <- f2(bt) + sum(diags2 * vbt^2)
+  var_term <- 1/lambda^2 * (p + sum(diags)) - 1/lambda * (p + sum(diags2))
+  bias_term + var_term
+}
+
+bt <- rnorm(10); X <-randn(5, 10); lambda <- 1 
+ridge_error_trials(bt, X, lambda, mc.reps = 2000)
+ridge_error_theory(bt, X, lambda)
