b125a322e48d14c5a4318b7f1118e5ab68c87785
Charles Zheng <snarles@stanford.edu>
2015-10-22 06:57:48 -0700

    explain this, edgar

---
+++ decision_theory/minimax_lambda_ridge.R
@@ -0,0 +1,36 @@
+
+
+mI <- function(z, gamma) {
+  -((z + gamma - 1) + sqrt((z + gamma-1)^2 - 4 * gamma * z))/
+    (2 * gamma * z)
+}
+mId <- function(z, gamma) {
+  -mI(z, gamma)/z + 1/(2 * gamma * z) *
+    (-1 - (z- gamma - 1)/sqrt((z + gamma - 1)^2 - 4 * gamma*z))
+}
+
+
+z <- -rexp(1); gamma <- rexp(1)
+mId(z, gamma)
+numDeriv::grad(function(z) mI(z, gamma), z)
+
+risk <- function(alpha2, gamma, lambda) {
+  1 + gamma * mI(-lambda, gamma) + lambda * (gamma - lambda * alpha2) * mId(-lambda, gamma)
+}
+risk_ <- function(alpha2, gamma) {
+  ff <- function(lambda) risk(alpha2, gamma, lambda)
+  ff
+}
+opt_risk <- function(alpha2, gamma, naive = FALSE) {
+  if (naive) {
+    ff <- function(lambda) risk(alpha2, gamma, lambda)
+    res <- optimise(ff, c(1e-5,1e5))
+    return(res$objective)
+  }
+  .5 * (1 + (gamma-1)/gamma * alpha2 + sqrt((1-(gamma-1)/gamma*alpha2)^2 + 4*alpha2))
+}
+
+alpha2 <- rexp(1); gamma <- rexp(1)
+risk(alpha2, gamma, gamma/alpha2)
+risk(alpha2, gamma, 2 * gamma/alpha2)
+
