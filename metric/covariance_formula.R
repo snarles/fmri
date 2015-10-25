21a9c0f12d5a700b3ccdfaea45deb065dbddc725
snarles <snarles@stanford.edu>
2015-10-25 14:39:46 -0700

    correlation formula

--- metric/covariance_formula.R
+++ metric/covariance_formula.R
@@ -2,12 +2,15 @@ library(pracma)
 
 TR <- function(A) sum(diag(A))
 cov_formula <- function(Sigma1, Sigma2) 2 * TR(Sigma1 %*% Sigma2)
+cor_formula <- function(Sigma1, Sigma2)
+  TR(Sigma1 %*% Sigma2)/sqrt(TR(Sigma1) * TR(Sigma2))
+
 
 Sigma1 <- cov(randn(10, 5))
 Sigma2 <- cov(randn(10, 5))
-nms <- do.call(rbind,lapply(1:1e3, function(i) {
+nms <- do.call(rbind,lapply(1:1e5, function(i) {
   z <- rnorm(5)
   c(t(z) %*% Sigma1 %*% z, t(z) %*% Sigma2 %*% z)}))
 
-cov(nms[, 1], nms[, 2])
-cov_formula(Sigma1, Sigma2)
+c(cov(nms[, 1], nms[, 2]), cov_formula(Sigma1, Sigma2))
+c(cor(nms[, 1], nms[, 2]), cor_formula(Sigma1, Sigma2))
