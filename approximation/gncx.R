ae4226fe7211e0292e79a937f09c28f11b0e0e20
snarles <snarles@stanford.edu>
2015-10-28 10:55:09 -0700

    got markov bounds... but are they good?

--- approximation/gncx.R
+++ approximation/gncx.R
@@ -48,32 +48,69 @@ finv <- function(fs, cs, x) {
   Re(rowSums(vals2 * diffs))/(2 * pi)
 }
 
+## exponential bound on Pr[<x]
+mb_gchisq <- function(tt, x, Sigma, mu) {
+  ms <- exp(mgf_gchisq(tt, Sigma, mu, TRUE))
+  ms/exp(tt * x)
+}
+
+## derivative of log. expo. bound wrt t
+prox_dleb <- function(tt, x, Sigma, mu) {
+  p <- dim(Sigma)[1]
+  res <- eigen(Sigma); ls <- res$values
+  nu <- as.numeric(t(res$vectors) %*% mu)
+  u <- 1/tt
+  # actual
+  sum(ls*(1 + nu^2)/(1-2*ls*tt)) + sum(2*nu^2*ls^2*tt/(1 - 2*ls*tt)^2) - x
+  # second-order
+  (-p/2)*u + sum((1+2*nu+3*nu^2)/ls)/4*u^2 - x
+  # first-order
+  (-p/2) * u - x
+}
+
+## exponential bound on log Pr[<x] plugging in t = -p/2x
+# lmb_gchisq <- function(x, Sigma, mu) {
+#   log(mb_gchisq(-.5*dim(Sigma)[1]/x, x, Sigma, mu))
+# }
+
+lmb_gchisq <- function(x, Sigma, mu) {
+    p <- dim(Sigma)[1]
+    res <- eigen(Sigma); ls <- res$values
+    nu <- as.numeric(t(res$vectors) %*% mu)
+    -1/2 * sum(log(1 + ls*p/x)) + p/2 * 
+      (1 - sum(nu^2*ls/(1 + ls*p/x))/x)
+}
+
+
+
 ####
 ##  Tests
 ####
 
 mu <- rnorm(5)
 Sigma <- cov(randn(10, 5))
-s1 <- rgchisq0(1e6, Sigma, mu)
-s2 <- rgchisq(1e6, Sigma, mu)
-plot(sort(s1), sort(s2), type = 'l')
-abline(0, 1, col = "red")
-
-.2 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
-
-fs <- (seq(-2, 2, by = 0.005) + 0.005 * runif(801)) * 1i
+#s1 <- rgchisq0(1e6, Sigma, mu)
+#s2 <- rgchisq(1e6, Sigma, mu)
+#cdf0 <- function(x) mean(s1 < x)
+#plot(sort(s1), sort(s2), type = 'l')
+#abline(0, 1, col = "red")
+#.2 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
+fs <- seq(-3, 3, by = 0.002) * 1i
 cs <- mgf_gchisq(fs, Sigma, mu)
-xs <- seq(0, 80, by = 0.5)
+#plot(Im(fs), Re(cs), type = "l")
+#plot(Im(fs), Im(cs), type = "l")
+xs <- seq(0, 1, length.out = 1e3)
 ps <- finv(fs, cs, xs)
-10 %>% {c(sum(ps[xs < .])/sum(ps), mean(s1 < .))}
+#10 %>% {c(sum(ps[xs < .])/sum(ps), mean(s1 < .))}
+cdf <- function(x) sum(ps[xs < x]) * (xs[2]-xs[1])
+x <- .5
+tt <- -seq(0,4/x,length.out=100); pus <- mb_gchisq(tt, x, Sigma, mu)
+plot(tt, log(pus), type = "l", ylim = c(log(cdf(x)) - 1, 0))
+abline(log(cdf(x)), 0)
+c(lmb_gchisq(x,Sigma,mu), log(min(pus)), log(cdf(x)))
 
-fs <- 5 * fs
-cs <- 1/(1 - 2*fs)^(p/2)
-
-
-plot(Im(fs), Re(cs), type = "l")
-plot(Im(fs), Im(cs), type = "l")
-finv(fs, cs, 1)
-dchisq(1:10, p)
-
-
+## test derivatives of log expo bound
+x <- 0.1
+tt0 <- -5
+numDeriv::grad(function(tt) mgf_gchisq(tt, Sigma, mu, TRUE) - tt*x, tt0)
+prox_dleb(tt0, x, Sigma, mu)
