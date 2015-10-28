7da2149a7bda2308e5d85c5dbfd965de8301a753
snarles <snarles@stanford.edu>
2015-10-28 08:47:11 -0700

    mgf of gncx

--- approximation/gncx.R
+++ approximation/gncx.R
@@ -19,6 +19,15 @@ rgchisq <- function(n, Sigma, mu) {
   colSums(x^2)
 }
 
+mgf_gchisq <- function(t, Sigma, mu, log = FALSE) {
+  p <- dim(Sigma)[1]
+  res <- eigen(Sigma); ls <- res$values
+  nu <- as.numeric(t(res$vectors) %*% mu)
+  ans <- -1/2 * sum(log(1 - 2*t*ls)) + sum(nu^2*ls*t/(1 - 2*t*ls))
+  if (log) return(ans)
+  exp(ans)
+}
+
 ####
 ##  Tests
 ####
@@ -29,3 +38,5 @@ s1 <- rgchisq0(1e6, Sigma, mu)
 s2 <- rgchisq(1e6, Sigma, mu)
 plot(sort(s1), sort(s2), type = 'l')
 abline(0, 1, col = "red")
+
+-1 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
