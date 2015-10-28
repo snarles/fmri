58bccf73c41e5c9e2c24b157d5dd90215abf7846
snarles <snarles@stanford.edu>
2015-10-28 09:24:50 -0700

    added fourier inversion

--- approximation/gncx.R
+++ approximation/gncx.R
@@ -19,13 +19,33 @@ rgchisq <- function(n, Sigma, mu) {
   colSums(x^2)
 }
 
-mgf_gchisq <- function(t, Sigma, mu, log = FALSE) {
+mgf_gchisq <- function(tt, Sigma, mu, log = FALSE) {
   p <- dim(Sigma)[1]
+  tt <- t(t(tt))
   res <- eigen(Sigma); ls <- res$values
   nu <- as.numeric(t(res$vectors) %*% mu)
-  ans <- -1/2 * sum(log(1 - 2*t*ls)) + sum(nu^2*ls*t/(1 - 2*t*ls))
-  if (log) return(ans)
-  exp(ans)
+  tM <- repmat(tt, 1, p)
+  lM <- repmat(t(ls), length(tt), 1)
+  nM <- repmat(t(nu), length(tt), 1)
+  if (log) {
+    temp <- -1/2 * log(1 - 2*tM*lM) + (nM^2*lM*tM/(1 - 2*tM*lM))
+    return(rowSums(temp))
+  } else {
+    temp <- 1/sqrt(1 - 2*tM*lM)*exp(nM^2*lM*tM/(1 - 2*tM*lM))
+    return(apply(temp, 1, prod))
+  }
+}
+
+## fourier inversion
+finv <- function(fs, cs, x) {
+  diffs <- Im(fs)[-1] - Im(fs)[-length(fs)]
+  fM <- repmat(t(fs), length(x), 1)
+  cM <- repmat(t(cs), length(x), 1)
+  xM <- repmat(t(t(x)), 1, length(fs))
+  vals <- exp(-fM * xM) * cM
+  vals2 <- (vals[, -1, drop = FALSE] 
+            + vals[, -length(fs), drop = FALSE])/2
+  Re(rowSums(vals2 * diffs))/(2 * pi)
 }
 
 ####
@@ -39,4 +59,21 @@ s2 <- rgchisq(1e6, Sigma, mu)
 plot(sort(s1), sort(s2), type = 'l')
 abline(0, 1, col = "red")
 
--1 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
+.2 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
+
+fs <- (seq(-2, 2, by = 0.005) + 0.005 * runif(801)) * 1i
+cs <- mgf_gchisq(fs, Sigma, mu)
+xs <- seq(0, 80, by = 0.5)
+ps <- finv(fs, cs, xs)
+10 %>% {c(sum(ps[xs < .])/sum(ps), mean(s1 < .))}
+
+fs <- 5 * fs
+cs <- 1/(1 - 2*fs)^(p/2)
+
+
+plot(Im(fs), Re(cs), type = "l")
+plot(Im(fs), Im(cs), type = "l")
+finv(fs, cs, 1)
+dchisq(1:10, p)
+
+
