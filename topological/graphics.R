f9d510ffc7617b3afd3c26da1aab8397052b4faa
snarles <snarles@stanford.edu>
2015-11-10 15:07:19 -0800

    tweak circ confine

--- topological/graphics.R
+++ topological/graphics.R
@@ -1,4 +1,5 @@
 library(lineId)
+library(magrittr)
 
 ## grf on points with coordinates x, cov = exp(-lambda d(x_i, x_j)^2/2)
 grf_grid <- function(xs, lambda = 2, k.basis = 5) {
@@ -18,26 +19,31 @@ grf_grid <- function(xs, lambda = 2, k.basis = 5) {
   list(vals = vals, d1 = d1, d2 = d2)
 }
 
-circ_confine <- function(xs, vals, d1, d2) {
-  gfunc <- (rowSums(xs^2) - 1)^2
-  g1 <- 4 * (rowSums(xs^2) - 1) * xs[, 1]
-  g2 <- 4 * (rowSums(xs^2) - 1) * xs[, 2]
+circ_confine <- function(xs, vals, d1, d2, pow = 1) {
+  xtx <- rowSums(xs^2)
+  gfunc <- (xtx^pow - 1)^2
+  g1 <- 4 *(xtx - 1) * pow * xtx^(pow-1) * xs[, 1]
+  g2 <- 4 *(xtx - 1) * pow * xtx^(pow-1) * xs[, 2]
+  gfunc[xtx > 1] <- 0;
+  g1[xtx > 1] <- 0; g2[xtx > 1] <- 0  
   new_vals <- vals * gfunc
   new_d1 <- vals * g1 + gfunc * d1
   new_d2 <- vals * g2 + gfunc * d2
   list(vals = new_vals, d1 = new_d1, d2 = new_d2)
 }
 
+ctp <- function(vals, d1, d2) {
+  contour(xseq, xseq, matrix(Re(vals), nrow=length(xseq) ), lwd = 2)
+  contour(xseq, xseq, matrix(Re(d1), nrow=length(xseq)) , add=TRUE, col="red")
+  contour(xseq, xseq, matrix(Re(d2), nrow=length(xseq) ), add=TRUE, col="blue")  
+}
+
 xseq <- seq(-1, 1, by = 0.01)
 xs <- cbind(rep(xseq, length(xseq)), rep(xseq, each = length(xseq)))
-lambda <- 5
-k.basis <- 5
-zattach(grf_grid(xs, lambda, k.basis))
-contour(xseq, xseq, matrix(Re(vals), nrow=length(xseq) ), lwd = 2)
-contour(xseq, xseq, matrix(Re(d1), nrow=length(xseq)) , add=TRUE, col="red")
-contour(xseq, xseq, matrix(Re(d2), nrow=length(xseq) ), add=TRUE, col="blue")
+lambda <- 8
+k.basis <- 8
+res_grf <- grf_grid(xs, lambda, k.basis)
+res_grf %$% ctp(vals, d1, d2)
 
-zattach(circ_confine(xs, vals, d1, d2))
-contour(xseq, xseq, matrix(Re(vals), nrow=length(xseq) ), lwd = 2)
-contour(xseq, xseq, matrix(Re(d1), nrow=length(xseq)) , add=TRUE, col="red")
-contour(xseq, xseq, matrix(Re(d2), nrow=length(xseq) ), add=TRUE, col="blue")
+res_c <- res_grf %$% circ_confine(xs, vals, d1, d2, pow = 5)
+res_c %$% ctp(vals, d1, d2)
