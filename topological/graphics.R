3b634270958ddb203a5928e48e3acf99f94f1a5b
snarles <snarles@stanford.edu>
2015-11-10 15:47:53 -0800

    demo of measure-preserving/non-measure preserving

--- topological/graphics.R
+++ topological/graphics.R
@@ -16,7 +16,7 @@ grf_grid <- function(xs, lambda = 2, k.basis = 5) {
   vals <- (mat %*% zs)[, 1]
   d1 <- (dmat1 %*% zs)[, 1]
   d2 <- (dmat2 %*% zs)[, 1]
-  list(vals = vals, d1 = d1, d2 = d2)
+  list(vals = vals, d1 = d1, d2 = d2, zs = zs)
 }
 
 circ_confine <- function(xs, vals, d1, d2, pow = 1) {
@@ -38,12 +38,73 @@ ctp <- function(vals, d1, d2) {
   contour(xseq, xseq, matrix(Re(d2), nrow=length(xseq) ), add=TRUE, col="blue")  
 }
 
+cc_query_ <- function(zs, k.basis, pow) {
+  kseq <- (-k.basis):k.basis
+  kgrid <- cbind(rep(kseq, each=(2*k.basis + 1)), rep(kseq, (2*k.basis + 1)))
+  ansf <- function(x) {
+    ## rf vals and ds
+    ef_vals <- exp(1i * pi * kgrid %*% x)[, 1]
+    rf_val <- sum(zs * ef_vals)
+    rf_d1 <- sum(1i*pi*kgrid[,1] * zs * ef_vals)
+    rf_d2 <- sum(1i*pi*kgrid[,2] * zs * ef_vals)
+    ## confine vals and ds
+    xtx <- sum(x^2)
+    gfunc <- (xtx^pow - 1)^2
+    g1 <- 4 *(xtx - 1) * pow * xtx^(pow-1) * x[1]
+    g2 <- 4 *(xtx - 1) * pow * xtx^(pow-1) * x[2]
+    val <- gfunc * rf_val
+    d1 <- gfunc * rf_d1 + g1 * rf_val
+    d2 <- gfunc * rf_d2 + g2 * rf_val
+    c(val = val, d1 = d1, d2 = d2)
+  }
+  ansf
+}
+
+## GENERATE X GRID
 xseq <- seq(-1, 1, by = 0.01)
 xs <- cbind(rep(xseq, length(xseq)), rep(xseq, each = length(xseq)))
+
+## GENERATE RF
 lambda <- 8
 k.basis <- 8
 res_grf <- grf_grid(xs, lambda, k.basis)
 res_grf %$% ctp(vals, d1, d2)
 
-res_c <- res_grf %$% circ_confine(xs, vals, d1, d2, pow = 5)
+## CONFINE RF TO CIRCLE
+pow <- 5
+res_c <- res_grf %$% circ_confine(xs, vals, d1, d2, pow)
 res_c %$% ctp(vals, d1, d2)
+
+## CHECK QUERY FUNC
+query <- cc_query_(res_grf$zs, k.basis, pow)
+ind <- sample(which(rowSums(xs^2) < 1), 1)
+xs[ind, ]
+res_grf %$% c(vals[ind], d1[ind], d2[ind])
+res_c %$% c(vals[ind], d1[ind], d2[ind])
+query(xs[ind, ])
+
+## APPLY measure-preserving FLOW TO SPIRAL
+ts <- seq(0, 1, by = 0.001) * 2 * pi
+pts <- ((ts/pi)-1) * cbind(cos(20 * ts), sin(20 * ts))
+par(bg = "grey")
+plot(pts, xlim = c(-1,1), ylim = c(-1, 1), type = "l", lwd = 2)
+eps <- 0.01
+nits <- 50
+for (i in 1:nits) {
+  ds <- t(apply(pts, 1, query))
+  pts <- pts + eps * Re(cbind(ds[,3], -ds[, 2]))
+  if (i %% 10==0) lines(pts, col = rainbow(nits)[i], lwd = 2)
+}
+
+## APPLY non-measure-preserving FLOW TO SPIRAL
+ts <- seq(0, 1, by = 0.001) * 2 * pi
+pts <- ((ts/pi)-1) * cbind(cos(20 * ts), sin(20 * ts))
+par(bg = "grey")
+plot(pts, xlim = c(-1,1), ylim = c(-1, 1), type = "l", lwd = 2)
+eps <- 0.01
+nits <- 50
+for (i in 1:nits) {
+  ds <- t(apply(pts, 1, query))
+  pts <- pts + eps * Re(cbind(ds[,2], ds[, 3]))
+  if (i %% 10==0) lines(pts, col = rainbow(nits)[i], lwd = 2)
+}
