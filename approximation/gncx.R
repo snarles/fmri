70b42d0f04ded55d3fe5cf9ce85605246bac8f42
snarles <snarles@stanford.edu>
2015-11-13 10:32:07 -0800

    checking lower bound stuff

--- approximation/gncx.R
+++ approximation/gncx.R
@@ -110,20 +110,62 @@ cap_lb_ <- function(x, Sigma, mu) {
   #c(nSn, t(mu) %*% solve(Sigma, mu)/f2(mu))
   zh <- sqrt(x/nSn) * (nun/ls)
   h <- sqrt(x * nSn)
-  m0 <- sqrt(f2(nu, zh))
   ## Volume of ellipse
   vc <- log_vsph(p) + (p/2)*log(x) - (1/2)*sum(log(ls))
   #c(h, sum(zh * nun))
+  d0 <- sqrt(f2(h * nun, zh))
   ## Function handle
   ff <- function(u) {
     ## volume of cap
     cv <- vc + pbeta(u, (p-1)/2, (p-1)/2, log.p=TRUE)
-    ml <- m0 + sqrt(x)/sqrt(min(ls)) * sqrt(1-(1-u)^2) + 2*u*h
+    ml <- (sqrt(f2(nu)) + h*(1-2*u))^2 +  (d0 + sqrt(x/min(ls))*sqrt(1-(1-u)^2))^2
     dens <- -(p/2) * log(2*pi) - (ml^2)/2
     dens + cv
   }
   ff
 }
+
+####
+##  Check cap volume and density
+####
+
+p <- 3
+mu <- rnorm(p)
+Sigma <- cov(randn(3*p, p)) + 10 * eye(p)
+s1 <- rgchisq(1e6, Sigma, mu)
+x <- s1[100]
+res <- eigen(Sigma); ls <- res$values
+nu <- as.numeric(t(res$vectors) %*% mu)
+cube_l_min <- 2 * sqrt(x/min(ls))
+
+mc.s <- 1e5
+cube_l <- 1.1 * cube_l_min
+cube_vol <- cube_l^p
+pts <- cube_l * matrix(runif(mc.s * p) - .5, mc.s, p)
+## check boundary inclusion
+flag <- TRUE
+minx <- Inf
+for (j in 1:p) {
+  for (ss in c(-1, 1)) {
+    pts2 <- pts; pts2[, j] <- cube_l * ss
+    nms <- apply(pts2, 1, function(v) t(v) %*% Sigma %*% v)
+    minx <- pmin(minx, min(nms))
+    if (min(nms) < x) flag <- FALSE
+  }
+}
+list(flag, minx, minx/x)
+
+
+## check volume
+mc.s <- 1e7
+pts <- cube_l * matrix(runif(mc.s * p) - .5, mc.s, p)
+#nms <- apply(pts, 1, function(v) t(v) %*% Sigma %*% v)
+nms <- rowSums((pts %*% sqrtm(Sigma))^2)
+  # empirical volume
+mean(nms < x) * cube_vol
+  # computed volume
+exp(log_vsph(p) - .5 * sum(log(ls)) + (p/2) * log(x))
+
 
 
 
@@ -131,40 +173,15 @@ cap_lb_ <- function(x, Sigma, mu) {
 ##  Tests
 ####
 
-# p <- 2;
-# mu <- rnorm(p)
-# Sigma <- cov(randn(2*p, p))
-# #s1 <- rgchisq0(1e6, Sigma, mu)
-# s1 <- rgchisq(1e6, Sigma, mu)
-# cdf <- function(x) sum(s1 < x)/length(s1)
-# cap_par <- 0.02
-# s1[50] %>% {c(cap_lb_(., Sigma, mu)(cap_par), log(cdf(.)), lmb_gchisq(., Sigma, mu))}
-# 
-# x <- s1[50]
-# ff <- cap_lb_(x, Sigma, mu)
-# (1:100/1000) %>% plot(., sapply(., ff), type = "l")
+p <- 3;
+mu <- rnorm(p)
+Sigma <- cov(randn(2*p, p))
+#s1 <- rgchisq0(1e6, Sigma, mu)
+s1 <- rgchisq(1e6, Sigma, mu)
+cdf <- function(x) sum(s1 < x)/length(s1)
+cap_par <- 0.01
+s1[50] %>% {c(cap_lb_(., Sigma, mu)(cap_par), log(cdf(.)), lmb_gchisq(., Sigma, mu))}
 
-# #s2 <- rgchisq(1e6, Sigma, mu)
-# #cdf0 <- function(x) mean(s1 < x)
-# #plot(sort(s1), sort(s2), type = 'l')
-# #abline(0, 1, col = "red")
-# #.2 %>%{c(mean(exp(.*s1)),mean(exp(.* s2)),mgf_gchisq(.,Sigma,mu))}
-# fs <- seq(-3, 3, by = 0.002) * 1i
-# cs <- mgf_gchisq(fs, Sigma, mu)
-# #plot(Im(fs), Re(cs), type = "l")
-# #plot(Im(fs), Im(cs), type = "l")
-# xs <- seq(0, 1, length.out = 1e3)
-# ps <- finv(fs, cs, xs)
-# #10 %>% {c(sum(ps[xs < .])/sum(ps), mean(s1 < .))}
-# cdf <- function(x) sum(ps[xs < x]) * (xs[2]-xs[1])
-# x <- .5
-# tt <- -seq(0,4/x,length.out=100); pus <- mb_gchisq(tt, x, Sigma, mu)
-# plot(tt, log(pus), type = "l", ylim = c(log(cdf(x)) - 1, 0))
-# abline(log(cdf(x)), 0)
-# c(lmb_gchisq(x,Sigma,mu), log(min(pus)), log(cdf(x)))
-# 
-# ## test derivatives of log expo bound
-# x <- 0.1
-# tt0 <- -5
-# numDeriv::grad(function(tt) mgf_gchisq(tt, Sigma, mu, TRUE) - tt*x, tt0)
-# prox_dleb(tt0, x, Sigma, mu)
+x <- s1[50]
+ff <- cap_lb_(x, Sigma, mu)
+(1:1e3/1e7) %>% plot(., sapply(., ff), type = "l")
