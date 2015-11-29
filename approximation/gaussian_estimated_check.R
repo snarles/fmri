9680a875cf68aafafe9d57d1fb4e705bf2e20a03
snarles <snarles@stanford.edu>
2015-11-29 15:19:53 -0800

    updated notes

--- approximation/gaussian_estimated_check.R
+++ approximation/gaussian_estimated_check.R
@@ -13,8 +13,10 @@ source("approximation/gncx.R")
 TR <- function(a) sum(diag(a))
 TR2 <- function(a) sum(diag(a %*% a))
 
+mc.reps <- 1e4
+
 cc <- 10
-p <- 100; r <- 2; K <- 1000
+p <- 10; r <- 100; K <- 3
 Omega <- 0.5 * cov(randn(20*p, p)) + 1000 * eye(p)
 Omega <- Omega * TR(solve(Omega))/cc
 c(TR(solve(Omega)), TR2(solve(Omega)))
@@ -27,21 +29,40 @@ A <- solve(eye(p) + OmegaH - solve(eye(p) + XiH))
 B <- solve(eye(p) + XiH)
 
 p <- dim(Omega)[1]
+
+nms_s <- zeros(mc.reps, K)
+y_s <- zeros(mc.reps, p)
+Bmu_star_s <- zeros(mc.reps, p)
+Bmu2_s <- zeros(mc.reps, p)
+
 Ah <- sqrtm(A)
-mus <- randn(K, p)
-mu_hs <- mus + mvrnorm(K, mu = rep(0, p), Sigma = Xi)
-ys <- mus + mvrnorm(K, mu = rep(0, p), Sigma = Omega)
-y <- ys[1, ]
-Bmu_hs <- mu_hs %*% t(B)
-delts <- t(t(Bmu_hs - y))
-scores <- rowSums((delts %*% Ah)^2)
-nms <- rowSums((Bmu_hs %*% Ah)^2)
-c(mean(nms), TR(A %*% B %*% (eye(p) + Xi) %*% t(B)))
-c(var(nms), 2 * TR2(A %*% B %*% (eye(p) + Xi) %*% t(B)))
+for (i in 1:mc.reps) {
+  mus <- randn(K, p)
+  mu_hs <- mus + mvrnorm(K, mu = rep(0, p), Sigma = Xi)
+  ys <- mus + mvrnorm(K, mu = rep(0, p), Sigma = Omega)
+  y <- ys[1, ]
+  Bmu_hs <- mu_hs %*% t(B)
+  delts <- t(t(Bmu_hs) - y)
+  dA <- delts %*% Ah
+  scores <- rowSums((delts %*% Ah)^2)
+  scores2 <- scores - f2(Ah %*% y)
+  # record results
+  nms_s[i, ] <- scores
+  y_s[i, ] <- y
+  Bmu_star_s[i, ] <- Bmu_hs[1, ]
+  Bmu2_s[i, ] <- Bmu_hs[2, ]
+}
 
+m_emp <- colMeans(nms_s)
+m_the <- rep(TR(A %*% (eye(p) + Omega + B %*% (eye(p) + Omega/r) %*% B)), K)
+m_the[1] <- m_the[1] - 2 * TR(A %*% B)
 
-dim(mu_hs)
-
-lbls <- knn(mu_hs %*% t(B) %*% Ah, ys %*% Ah, cl=1:K)
-sum(lbls != 1:K)/K
-
+v_emp <- cov(nms_s)
+c(v_emp[1,1],
+  2 * TR2(A %*% (eye(p) + Omega + B %*% (eye(p) + Omega/r) %*% B - 2 * B)))
+c(v_emp[1,2],
+  2 * TR2(A %*% (eye(p) + Omega - B)))
+c(v_emp[2,2],
+  2 * TR2(A %*% (eye(p) + Omega + B %*% (eye(p) + Omega/r) %*% B)))
+c(v_emp[2,3],
+  2 * TR2(A %*% (eye(p) + Omega)))
