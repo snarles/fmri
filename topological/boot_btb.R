0db4e55b95def58a4cb7c50d5a2b682486a78e87
Charles Zheng <snarles@stanford.edu>
2015-12-02 20:02:32 -0800

    bootstrap distribution

---
+++ topological/boot_btb.R
@@ -0,0 +1,66 @@
+library(pracma)
+library(MASS)
+library(lineId)
+library(parallel)
+#library(ade4)
+
+####
+##  Double bootstrap method
+####
+
+boot_stat <- function(XA, YA, XB, YB) {
+  BA <- solve(t(XA) %*% XA, t(XA) %*% YA)
+  BB <- solve(t(XB) %*% XB, t(XB) %*% YB)
+  f2(BA %*% t(BA) - BB %*% t(BB))
+}
+
+inner_boot <- function(boot_stat, XA, YA, XB, YB, rep.in = 1000) {
+  vals <- numeric(rep.in)
+  st <- boot_stat(XA, YA, XB, YB)
+  for (i in 1:rep.in) {
+    indA <- sample(nA, nA, replace = TRUE)
+    indB <- sample(nB, nB, replace = TRUE)
+    vals[i] <- boot_stat(XA[indA, ], YA[indA, ], XB[indB, ], YB[indB, ])
+  }
+  st/sd(vals)
+}
+
+double_boot <- function(boot_stat, XA, YA, XB, YB, rep.out = 1000, rep.in = rep.out) {
+  nA <- dim(YA)[1]; nB <- dim(YB)[1]
+  outer.vals <- numeric(rep.out)
+  for (i in 1:rep.out) {
+    indA <- sample(nA, nA, replace = TRUE)
+    indB <- sample(nB, nB, replace = TRUE)
+    outer.vals[i] <- inner_boot(boot_stat, XA[indA, ], YA[indA, ], XB[indB, ], YB[indB, ], rep.in)
+  }
+  outer.vals
+}
+
+####
+## TEST
+####
+
+nA <- 40; nB <- 50; p <- 2; q <- 10
+XA <- randn(nA, p)
+XB <- randn(nB, p)
+G0 <- randn(p, q); G1 <- randn(p, q)
+GA <- svd(randn(q, q))$u
+GB <- svd(randn(q, q))$u
+# non-null case
+BA0 <- G0 %*% GA; BB0 <- G0 %*% GB
+# null case
+BA0 <- G0 %*% GA; BB0 <- G1 %*% GB
+
+SigmaA <- cov(randn(2 * q, q))
+SigmaB <- cov(randn(2 * q, q))
+truth <- list(G=G0 %*% t(G0), BA = BA0, BB=BB0)
+
+
+YA <- XA %*% BA0 + mvrnorm(nA, rep(0, q), SigmaA)
+YB <- XB %*% BB0 + mvrnorm(nB, rep(0, q), SigmaA)
+
+boot_stat(XA, YA, XB, YB)
+
+inner_boot(boot_stat, XA, YA, XB, YB)
+vals <- double_boot(boot_stat, XA, YA, XB, YB, rep.out = 100)
+hist(vals)
