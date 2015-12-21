f4d9c8229976dd19a18da0877e51d28c2edd801c
Charles Zheng <snarles@stanford.edu>
2015-12-21 20:28:55 +0000

    amazon script

--- topological/boot2.R
+++ topological/boot2.R
@@ -3,7 +3,7 @@
 ####
 
 library(parallel)
-MCC <- 3
+MCC <- 39
 source("topological/rsa_boot_source.R")
 
 p_value_dist <- function(A, B, SigmaX, SigmaY, nX, nY, theta, boot.reps, outer.reps = 100) {
@@ -16,18 +16,46 @@ p_value_dist <- function(A, B, SigmaX, SigmaY, nX, nY, theta, boot.reps, outer.r
   unlist(pvs)
 }
 
-pplot <- function(pvs) {
-  plot(1:length(pvs)/length(pvs), sort(pvs), ylim = c(0, 1), type = "l"); abline(0, 1)  
+pplot <- function(pvs, ...) {
+  plot(1:length(pvs)/length(pvs), sort(pvs), ylim = c(0, 1), type = "l", ...); abline(0, 1)  
 }
 
-p <- 5
-q <- 2
-sigma <- 20
-nX <- 30
-nY <- 30
-A_0 <- randn(p, q); B_0 <- svd(randn(p, p))$u %*% A_0
+p <- 20 # number of neurons
+q <- 2 # dimension of regressor
+nX <- 50
+nY <- 50
+boot.reps <- 1e3
+o.reps <- 5e2
 
-f2(A_0)
+f2a <- 10
+f2d <- 5
+A_0 <- randn(p, q)
+DD <- randn(p, q); DD <- sqrt(f2d) * DD/sqrt(f2(DD))
+A_0 <- sqrt(f2a) * A_0/sqrt(f2(A_0))
+B_0 <- svd(randn(p, p))$u %*% A_0
+B_1 <- B_0 + DD
 
-pvs <- p_value_dist(A_0, B_0, eye(q)/sigma, eye(q)/sigma, nX, nY, stat.T, 1e3, 100)
-pplot(pvs)
+## Null case
+
+pvs <- p_value_dist(A_0, B_0, eye(q), eye(q), nX, nY, stat.T, boot.reps, o.reps)
+pplot(pvs, ann = FALSE)
+title(paste("h0 T, p=",p,"q=",q,"nX=",nX,"nY=",nY,"B=",boot.reps),
+      sub = paste("f2a =", f2a))
+
+pvs <- p_value_dist(A_0, B_0, eye(q), eye(q), nX, nY, stat.S, boot.reps, o.reps)
+pplot(pvs, ann = FALSE)
+title(paste("h0 S, p=",p,"q=",q,"nX=",nX,"nY=",nY,"B=",boot.reps),
+      sub = paste("f2a =", f2a))
+
+## Alt case
+
+pvs <- p_value_dist(A_0, B_1, eye(q), eye(q), nX, nY, stat.T, boot.reps, o.reps)
+pplot(pvs, ann = FALSE)
+title(paste("h1 T, p=",p,"q=",q,"nX=",nX,"nY=",nY,"B=",boot.reps),
+      sub = paste("f2a =", f2a, "f2d=", f2d))
+
+pvs <- p_value_dist(A_0, B_1, eye(q), eye(q), nX, nY, stat.S, boot.reps, o.reps)
+pplot(pvs, ann = FALSE)
+title(paste("h1 S, p=",p,"q=",q,"nX=",nX,"nY=",nY,"B=",boot.reps),
+      sub = paste("f2a =", f2a, "f2d=", f2d))
+
