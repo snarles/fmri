249a46766cdb25ad1cb4e34b0dda9723baea4a07
Charles Zheng <snarles@stanford.edu>
2015-12-20 17:45:56 -0800

    rsa boot source

--- topological/rsa_boot_source.R
+++ topological/rsa_boot_source.R
@@ -159,25 +159,25 @@ inverse_bca_test <- function(res, theta, mc.reps = 1000) {
 ##  Demo
 ####
 
-p <- 5
-q <- 2
-SigmaX <- 3 * cov(randn(2*q, q)); SigmaY <- 3 * cov(randn(2 * q, q))
-A_0 <- randn(p, q); B_0 <- svd(randn(p, p))$u %*% A_0
-h0_small <- regression_data_model_(A_0, B_0, SigmaX, SigmaY)
-
-B_1 <- randn(p, q)
-h1_small <- regression_data_model_(A_0, B_1, SigmaX, SigmaY)
-
-dat <- h0_small(200, 200)
-mus <- sample_moments(dat)
-c(f2(mus$Ahat, A_0), f2(mus$Bhat, B_0))
-
-
-nX <- 100; nY <- 100; mc.reps = 1000
-res0 <- h0_small(nX, nY)
-res1 <- h1_small(nX, nY)
-
-c(inverse_bca_test(res0, stat.T, mc.reps), inverse_bca_test(res1, stat.T, mc.reps))
-c(inverse_bca_test(res0, stat.T.nm, mc.reps), inverse_bca_test(res1, stat.T.nm, mc.reps))
-c(inverse_bca_test(res0, stat.S, mc.reps), inverse_bca_test(res1, stat.S, mc.reps))
-c(inverse_bca_test(res0, stat.S.nm, mc.reps), inverse_bca_test(res1, stat.S.nm, mc.reps))
+# p <- 5
+# q <- 2
+# SigmaX <- 3 * cov(randn(2*q, q)); SigmaY <- 3 * cov(randn(2 * q, q))
+# A_0 <- randn(p, q); B_0 <- svd(randn(p, p))$u %*% A_0
+# h0_small <- regression_data_model_(A_0, B_0, SigmaX, SigmaY)
+# 
+# B_1 <- randn(p, q)
+# h1_small <- regression_data_model_(A_0, B_1, SigmaX, SigmaY)
+# 
+# dat <- h0_small(200, 200)
+# mus <- sample_moments(dat)
+# c(f2(mus$Ahat, A_0), f2(mus$Bhat, B_0))
+# 
+# 
+# nX <- 100; nY <- 100; mc.reps = 1000
+# res0 <- h0_small(nX, nY)
+# res1 <- h1_small(nX, nY)
+# 
+# c(inverse_bca_test(res0, stat.T, mc.reps), inverse_bca_test(res1, stat.T, mc.reps))
+# c(inverse_bca_test(res0, stat.T.nm, mc.reps), inverse_bca_test(res1, stat.T.nm, mc.reps))
+# c(inverse_bca_test(res0, stat.S, mc.reps), inverse_bca_test(res1, stat.S, mc.reps))
+# c(inverse_bca_test(res0, stat.S.nm, mc.reps), inverse_bca_test(res1, stat.S.nm, mc.reps))
