b7b79649917b999138adbe2f0d64613ad6170fde
snarles <snarles@stanford.edu>
2015-12-22 14:50:20 -0800

    found params for fig 2

--- info_theory_sims/sim3i_fig2.R
+++ info_theory_sims/sim3i_fig2.R
@@ -33,16 +33,3 @@ run_simulation(Bmat, m.folds, k.each, r.each, r.train)
 proc.time() - t1
 ## full-scale
 res <- run_simulations(Bmat, m.folds, k.each, r.each, r.train, mcc, data.reps)
-## display results
-c(mi_true = mi_true, mi_ls = est_ls['mc_b_ls'], apply(res, 2, median), abe = est_ls['abe'])
-apply(res[, 1:6] - mi_true, 2, summary)
-colSums((res[, 1:6] - mi_true)^2)/data.reps
-## save results
-packet <- list(Bmat = Bmat, m.folds = m.folds,
-               k.each = k.each, r.each = r.each, r.train = r.train,
-               mi_true, est_ls = est_ls, res = res,
-               mc.reps = mc.reps, mc.abe = mc.abe)
-allresults <- c(allresults, list(packet))
-
-
-save(allresults, file = 'info_theory_sims/save.Rdata')
