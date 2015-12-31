400a5cfcfac0d0d90819aa1854e8357a705c63ea
Charles Zheng <snarles@stanford.edu>
2015-12-31 21:07:40 +0000

    fig5

--- info_theory_sims/sim3i_fig5.R
+++ info_theory_sims/sim3i_fig5.R
@@ -22,8 +22,8 @@ t1 <- proc.time()
 (est_ls <- get_abe(Bmat, k.each, abe.each, mc.abe, mcc))
 proc.time() - t1
 ## data params
-N <- 160000
-ks <- c(5, 10, 15, 20, 25, 30, 35, 40)
+N <- 80000
+ks <- c(5, 10, 15, 20, 30, 40, 50, 60, 70, 80)
 ress <- lapply(ks, function(k) 
   run_simulations(Bmat, 1, k, floor(N/k), floor(0.5 * N/k), mcc, data.reps))
 #res <- run_simulations(Bmat, m.folds, k.each, r.each, r.train, mcc, data.reps)
@@ -33,11 +33,22 @@ save(ress, file = "info_theory_sims/fig5.Rdata")
 res2 <- do.call(rbind, ress)
 K <- rep(ks, each = data.reps)
 tab1 <- data.frame(I = res2[, 3], K = K)
+tab2 <- data.frame(I = res2[, 4], K = K)
+
 res3 <- do.call(cbind, ress)
 mses <- colSums((res3 - mi_true)^2)/data.reps
 mses_ls <- mses[(1:length(ks)) * 9 - 6]
+mses_0 <- mses[(1:length(ks)) * 9 - 5]
 
-boxplot(I ~ K, data = tab1, ylim = c(0.5, 2.8))
+
+boxplot(I ~ K, data = tab1, ylim = c(0.5, 2.8), xlab = "K")
 abline(mi_true, 0, lty = 2, col = "red", lwd = 3)
-text(1, 2.8, "RMSE x 100 = ", col = "red", cex = 0.8)
+text(1.5, 2.8, "RMSE x 100 = ", col = "red", cex = 0.8)
 for (i in 1:length(ks)) text(i, 2.5, format(round(mses_ls[i] * 1e2, 1)), col = "red")
+#abline(est_ls[2], 0)
+
+boxplot(I ~ K, data = tab2, ylim = c(0.5, 2.8), xlab = "K")
+abline(mi_true, 0, lty = 2, col = "red", lwd = 3)
+text(1.5, 2.8, "RMSE x 100 = ", col = "red", cex = 0.8)
+for (i in 1:length(ks)) text(i, 2.5, format(round(mses_0[i] * 1e2, 1)), col = "red")
+
