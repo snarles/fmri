359a94a378d9c51f353675f9823a6b4da357581d
Charles Zheng <snarles@stanford.edu>
2016-02-17 15:30:23 -0800

    differing variances does cause problems!!

--- one_dim_case.R
+++ one_dim_case.R
@@ -67,6 +67,56 @@ sampling_dist <- function(sampler, theta, nX, nY, mc.reps = 1e3, samples = FALSE
   list(bias = bias, sdv = sdv, skew = skew)
 }
 
+jackknife <- function(res, theta) {
+  theta0 <- theta(res)
+  n <- dim(res)[1]
+  thetas <- list()
+  for (i in 1:n) {
+    dat2 <- res[-i, , drop = FALSE]
+    thetas[[i]] <- theta(dat2)
+  }
+  thetas <- do.call(cbind, thetas)
+  theta_bar <- rowMeans(thetas)
+  diffs <- thetas - theta_bar
+  bias <- (n-1) * (theta_bar - theta0)
+  sdv <- sqrt((n-1) * rowMeans((thetas - theta_bar)^2))
+  skew <- rowSums(diffs^3)/(rowSums(diffs^2))^(3/2)  
+  list(bias = bias, sdv = sdv, skew = skew)
+}
+
+inv_alpha_bca <- function(alpha_bca, z0, acc) {
+  if (alpha_bca == 1) {
+    return(1)
+  }
+  za <- qnorm(alpha_bca)
+  pnorm(
+    ((1 - acc * z0) * (za - z0) - z0)/
+      (1 + acc * (za - z0))
+  )
+}
+
+inverse_bca_test <- function(res, theta, mc.reps = 1000) {
+  jj <- jackknife(res, theta)
+  acc <- jj$skew/6
+  boot_s1 <- boot_sampler(res)
+  nX <- sum(res[, 1] == 0); nY <- sum(res[, 1] == 1)
+  thetas <- sampling_dist(boot_s1, theta, nX, nY, mc.reps, TRUE)
+  theta0 <- theta(res)
+  z0s <- sapply(1:length(theta0), function(i) {
+    qnorm(sum(thetas[i, ] < theta0[i])/mc.reps)
+  })
+  pvs <- sapply(1:length(theta0), function(i) {
+    v <- thetas[i, ]
+    p1 <- (sum(v > 0) + 1)/(mc.reps + 1)
+    p2 <- (sum(v < 0) + 1)/(mc.reps + 1)
+    p1_mod <- inv_alpha_bca(p1, z0s[i], acc[i])
+    p2_mod <- inv_alpha_bca(p2, -z0s[i], acc[i])
+    2 * pmin(p1_mod, p2_mod)
+  })
+  min(pvs) * length(pvs)
+}
+
+
 
 ####
 ##  Test out the functions
@@ -115,3 +165,17 @@ layout(t(t(1:2))); hist(vals1); hist(vals2)
 vals1 <- sampling_dist(h1, stat.diff, nX, nY, mc.reps, TRUE)
 vals2 <- sampling_dist(boot_sampler(h1(nX, nY)), stat.diff, nX, nY, mc.reps, TRUE)
 layout(t(t(1:2))); hist(vals1); hist(vals2)
+
+####
+##  How does inverse BCA do?
+####
+mc.reps = 1000
+nX <- 100; nY <- nX 
+res0 <- h0(nX, nY)
+res0v <- h0v(nX, nY)
+res1 <- h1(nX, nY)
+
+c(inverse_bca_test(res0, stat.diff, mc.reps), 
+  inverse_bca_test(res0v, stat.diff, mc.reps),
+  inverse_bca_test(res1, stat.diff, mc.reps))
+
