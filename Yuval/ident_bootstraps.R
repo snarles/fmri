c78c419a2b2d70fd13cc7bbb2874cd76d3e58882
snarles <snarles@stanford.edu>
2016-03-03 20:00:00 -0800

    oops, features carry too much information

--- Yuval/ident_bootstraps.R
+++ Yuval/ident_bootstraps.R
@@ -20,6 +20,7 @@ newCondVar =genCondVar(1:10409,usevox)
 # make variance symmetric
 newCondVarS = (newCondVar + t(newCondVar))/2
 ridgeinv = solve(newCondVarS + diag(length(usevox))*3)
+bestMatch(testpred,testY,'MSE',invCov = ridgeinv)
 
 ## make random CV splits
 mc.reps <- 10
@@ -33,26 +34,31 @@ for (split.ind in 1:mc.reps) {
   t1 <- proc.time()
   tr.inds <- train_splits[split.ind, ]
   tpred.new <- matrix(0, nte, nvox)
+  fullbeta = matrix(0,nc=nvox,nr=ncol(c_ttF))
   for (vox.no in 1:nvox) {
     vox.ind <- usevox[vox.no]
     rule <- vox_prediction_rules[[vox.ind]]
     feat.inds <- which(rule$beta_coef != 0)
     if (length(feat.inds) > 0) {
-      c_ttSub <- c_ttF[, feat.inds]
+      c_ttSub <- c_ttF[, feat.inds, drop = FALSE]
       teY <- ttY[!tr.inds, vox.ind]
       trY <- ttY[tr.inds, vox.ind]
-      teX <- c_ttSub[!tr.inds, ]
-      trX <- c_ttSub[tr.inds, ]
-      Yh <- teX %*% ginv(trX) %*% trY
+      teX <- c_ttSub[!tr.inds, , drop = FALSE]
+      trX <- c_ttSub[tr.inds, , drop = FALSE]
+      bt <- ginv(trX) %*% trY
+      Yh <- teX %*% bt
+      fullbeta[feat.inds, vox.no] <- bt
       tpred.new[, vox.no] <- Yh
     }
   }
+  voxcov = (t(ttY[tr.inds, usevox])%*%ttY[tr.inds, usevox])/(1750 - nte)
+  voxfeatcov = (t(ttY[tr.inds, usevox]) %*% c_ttF[tr.inds, ])/(1750 - nte)
+  voxCondCov = voxcov - voxfeatcov%*%fullbeta
+  newCondVarS = (newCondVar + t(newCondVar))/2
+  ridgeinv = solve(newCondVarS + diag(length(usevox))*3)
   score1 <- bestMatch(tpred.new, ttY[!tr.inds, ],'MSE',invCov = ridgeinv)
   results[split.ind, ] <- c(score1)
   (times[split.ind] <- (proc.time() - t1)["elapsed"])
 }
 
 
-bestMatch(validpred,validY[1:120,],'MSE',invCov = ridgeinv)
-bestMatch(testpred,testY[1:120,],'MSE',invCov = ridgeinv)
-bestMatch(testpred,testY[1:250,],'MSE',invCov = ridgeinv)
