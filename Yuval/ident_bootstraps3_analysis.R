4228b1ea8b3f327f77bbad1b97d8e2a8666c279c
snarles <snarles@stanford.edu>
2016-03-08 20:20:36 -0800

    visualization of results

---
+++ Yuval/ident_bootstraps3_analysis.R
@@ -0,0 +1,22 @@
+ms <- c(25, 50, 75, 100, 125, 150, 175, 200, 225, 250)
+
+ihatss <- list()
+nvoxs <- c(100, 200, 300, 400, 500, 600)
+
+for (nvox in nvoxs) {
+  ff <- paste0('Yuval/ident_bootstraps3_nvox', nvox, '.rds')
+  ihats <- readRDS(ff)
+  boxplot(ihats)
+  title(paste("nvox=", nvox))
+  ihatss <- c(ihatss, list(ihats))
+}
+
+bigmat <- do.call(cbind, ihatss)
+
+boxplot(bigmat, ylim = c(0, 9))
+for (i in 0:length(nvoxs))  {
+  abline(v = length(ms) * i + 0.5)
+  if (i > 0) {
+    text(length(ms) * i - length(ms)/2, 9, paste0("n=", nvoxs[i]))
+  }
+}
