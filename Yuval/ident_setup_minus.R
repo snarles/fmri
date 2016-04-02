fb593fbad1c0705f627b73e485be0a01bbb774cb
snarles <snarles@stanford.edu>
2016-04-02 11:59:56 -0700

    .

---
+++ Yuval/ident_setup_minus.R
@@ -0,0 +1,74 @@
+library(MASS)
+library(pracma)
+library(glmnet)
+library(lineId)
+
+#load('Yuval/v1_data.RData')
+#load('Yuval/trainData.RData') 
+#load('Yuval/pred_rules.RData')
+
+getPreds = function(dataF,ind_struct=vox_prediction_rules,voxind=usevox){
+  preds = matrix(0,nc = length(voxind),nr = nrow(dataF))
+  for (i in 1:length(voxind)){
+    preds[,i] = dataF %*% ind_struct[[voxind[i]]]$beta_coef
+  }
+  return (preds)
+}
+
+genCondVar = function(featind,voxind,ind_struct=vox_prediction_rules){
+  nvox = length(voxind)
+  voxcov = (t(trainY[,voxind])%*%trainY[,voxind])/(nrow(trainY))
+  voxfeatcov = (t(trainY[,voxind]) %*% c_trainF[,featind])/(nrow(trainY))
+  fullbeta = matrix(0,nc=length(voxind),nr=length(featind))
+  for (i in 1:length(voxind)){
+    fullbeta[,i] = ind_struct[[voxind[i]]]$beta_coef
+  }
+  voxCondCov = voxcov - voxfeatcov%*%fullbeta
+  return (voxCondCov)
+}
+
+bestMatch = function(preds, dataY, score,invCov = diag(length(usevox)), voxind=usevox){
+  npoints = nrow(preds)
+  chosen = numeric(nrow(dataY))
+  for (i in 1:nrow(dataY)){
+    if (score=='MSE'){
+      Yi = matrix(rep(dataY[i,voxind],npoints),nr=npoints,byrow=T)
+      ResYi = Yi - preds
+      chosen[i] = which.min(diag(ResYi %*% invCov %*% t(ResYi)))
+    }
+    else if (score=='COR'){
+      chosen[i] = which.max(apply(preds,1,cor,dataY[i,voxind]))
+    }
+    else if (score=='COV'){
+      chosen[i] = which.max(apply(preds,1,cov,dataY[i,voxind]))
+    }
+    if ((i%%20)==0){
+      cat(',')
+    }
+  }
+  return(sum(chosen==(1:nrow(dataY)))/nrow(dataY))
+}
+
+matchResults = function(preds, dataY, score,invCov = diag(length(usevox)), voxind=usevox, rankit = TRUE){
+  npoints = nrow(preds)
+  res <- list()
+  for (i in 1:nrow(dataY)){
+    if (score=='MSE'){
+      Yi = matrix(rep(dataY[i,voxind],npoints),nr=npoints,byrow=T)
+      ResYi = Yi - preds
+      res[[i]] = diag(ResYi %*% invCov %*% t(ResYi))
+    }
+    else if (score=='COR'){
+      res[[i]] = -apply(preds,1,cor,dataY[i,voxind])
+    }
+    else if (score=='COV'){
+      res[[i]] = -apply(preds,1,cov,dataY[i,voxind])
+    }
+    if ((i%%20)==0){
+      cat(',')
+    }
+  }
+  res <- do.call(rbind, res)
+  if (rankit) res <- t(apply(res, 1, rank))
+  res
+}
--- Yuval/replicate_pred.R
+++ Yuval/replicate_pred.R
@@ -2,47 +2,9 @@
 ##  Predictions with bootstrap
 ####
 
-getPreds = function(dataF,ind_struct=vox_prediction_rules,voxind=usevox){
-  preds = matrix(0,nc = length(voxind),nr = nrow(dataF))
-  for (i in 1:length(voxind)){
-    preds[,i] = dataF %*% ind_struct[[voxind[i]]]$beta_coef
-  }
-  return (preds)
-}
-
-genCondVar = function(featind,voxind,ind_struct=vox_prediction_rules){
-  nvox = length(voxind)
-  voxcov = (t(trainY[,voxind])%*%trainY[,voxind])/(nrow(trainY))
-  voxfeatcov = (t(trainY[,voxind]) %*% c_trainF[,featind])/(nrow(trainY))
-  fullbeta = matrix(0,nc=length(voxind),nr=length(featind))
-  for (i in 1:length(voxind)){
-    fullbeta[,i] = ind_struct[[voxind[i]]]$beta_coef
-  }
-  voxCondCov = voxcov - voxfeatcov%*%fullbeta
-  return (voxCondCov)
-}
-
-bestMatch = function(preds, dataY, score,invCov = diag(length(usevox)), voxind=usevox){
-  npoints = nrow(preds)
-  chosen = numeric(nrow(dataY))
-  for (i in 1:nrow(dataY)){
-    if (score=='MSE'){
-      Yi = matrix(rep(dataY[i,voxind],npoints),nr=npoints,byrow=T)
-      ResYi = Yi - preds
-      chosen[i] = which.min(diag(ResYi %*% invCov %*% t(ResYi)))
-    }
-    else if (score=='COR'){
-      chosen[i] = which.max(apply(preds,1,cor,dataY[i,voxind]))
-    }
-    else if (score=='COV'){
-      chosen[i] = which.max(apply(preds,1,cov,dataY[i,voxind]))
-    }
-    if ((i%%20)==0){
-      cat(',')
-    }
-  }
-  return(sum(chosen==(1:nrow(dataY)))/nrow(dataY))
-}
+source("Yuval/ident_setup_minus.R")
+ms <- c(25, 50, 75, 100, 125, 150, 175, 200, 225, 250)
+nm <- length(ms)
 
 ## create training split
 load("Yuval/v1_data.RData")
@@ -80,3 +42,14 @@ ridgeinv = solve(newCondVarS + diag(length(usevox))*3)
 bestMatch(validpred,validY[1:120,],'MSE',invCov = ridgeinv)
 bestMatch(testpred,testY[1:120,],'MSE',invCov = ridgeinv)
 bestMatch(testpred,testY[1:250,],'MSE',invCov = ridgeinv)
+
+
+scores <- matchResults(testpred, testY, 'MSE',invCov = ridgeinv, rankit = FALSE)
+ihat <- numeric(nm)
+for (ind in 1:nm) {
+  m <- ms[ind]
+  (p <- resample_misclassification(-scores, 1:250, m, replace = FALSE))
+  (p_prime <- rbinom(1, 250, p)/250)
+  (IH <- Ihat_LI(p_prime, m, 20))
+  ihats[ind] <- IH
+}
