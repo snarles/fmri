eb27451b6f3ce385081fc17e9320d7757cb78fbe
snarles <snarles@stanford.edu>
2016-04-24 16:55:37 -0700

    MLE might be ok, just converges slowly

--- extrapolation/mle_theory.R
+++ extrapolation/mle_theory.R
@@ -21,17 +21,24 @@ ps <- rbeta(k.comps, 1, 1) ## Ps
 prob.ps <-  rbeta(k.comps, 1, 1) ## prob of ps
 prob.ps <- prob.ps/sum(prob.ps)
 
-k <- 100 # size of binom
+k <- 20 # size of binom
 n <- 1000 # how many dudes you see = k * r
 Ys <- generate_sample(ps, prob.ps, k, n)
 Ymat <- cbind(k - Ys, Ys)
-hist(Ys)
+hist(Ys/k)
 library(mixtools)
 res <- multmixEM(Ymat, k = 10, verb = TRUE)
 est_moment(res, 2)
 pk_moment(ps, prob.ps, 2)
 est_moment(res, 3)
 pk_moment(ps, prob.ps, 3)
+
+est_moment(res, 30)
+pk_moment(ps, prob.ps, 30)
+mean((Ys/k)^30)
+
+
 mean((Ys/k)^2)
 
-
+rbind(res$theta[, 2][order(-res$lambda)], sort(res$lambda, decreasing = TRUE))
+rbind(ps, prob.ps)
