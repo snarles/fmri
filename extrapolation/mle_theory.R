2ba18959b4cd9df05d09db1519d5e229354c68cb
snarles <snarles@stanford.edu>
2016-04-24 16:45:08 -0700

    mixtools fail!!

--- extrapolation/mle_theory.R
+++ extrapolation/mle_theory.R
@@ -12,15 +12,26 @@ pk_moment <- function(ps, prob.ps, k) {
   sum(ps^k * prob.ps)
 }
 
-k.comps <- 10 ## number of components of mixture
+est_moment <- function(res, k) {
+  sum(res$theta[, 2]^k * res$lambda)
+}
+
+k.comps <- 3 ## number of components of mixture
 ps <- rbeta(k.comps, 1, 1) ## Ps
 prob.ps <-  rbeta(k.comps, 1, 1) ## prob of ps
 prob.ps <- prob.ps/sum(prob.ps)
 
-k <- 10 # size of binom
-n <- 40 # how many dudes you see = k * r
+k <- 100 # size of binom
+n <- 1000 # how many dudes you see = k * r
 Ys <- generate_sample(ps, prob.ps, k, n)
-hist(ys)
-
+Ymat <- cbind(k - Ys, Ys)
+hist(Ys)
+library(mixtools)
+res <- multmixEM(Ymat, rep(1/10, 10), theta = NULL)
+est_moment(res, 2)
 pk_moment(ps, prob.ps, 2)
+est_moment(res, 3)
 pk_moment(ps, prob.ps, 3)
+
+
+
