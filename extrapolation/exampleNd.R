56df7bbb90355130b46490659a5b46ff00310bc6
snarles <snarles@stanford.edu>
2016-04-26 12:45:59 -0700

    mle method may be OK

--- extrapolation/exampleNd.R
+++ extrapolation/exampleNd.R
@@ -3,16 +3,18 @@
 ####
 
 source("extrapolation/example2d.R")
+source("extrapolation/mle_theory.R")
+
 
 library(pracma)
 
 nX <- 200
-nY <- 400
-rk <- 3
+nY <- 500
+rk <- 10
 xfax <- randn(nX, rk)
 yfax <- randn(nY, rk)
 ipmat <- xfax %*% t(yfax)
-coefs <- c(-2, 1, -2, 3)
+coefs <- c(-2, 5, 0.1, 0.1)
 pmat <- exp(coefs[1] + coefs[2] * ipmat + coefs[3] * ipmat^2 + coefs[4] * ipmat)
 for (i in 1:10) {
   rs <- rowSums(pmat)
@@ -23,9 +25,20 @@ avg_mc_acc_naive(pmat, 20)
 avg_mc_acc_p(pmat, 20)
 avg_mc_acc_naive(pmat, 40)
 avg_mc_acc_p(pmat, 40)
-rankconv <- (apply(pmat, 2, rank) - 0.5)/nrow(pmat)
-plot(density(as.numeric(rankconv), weights = as.numeric(pmat)/sum(pmat)))
-ps <- sample(as.numeric(rankconv), 10000, replace = TRUE, prob = as.numeric(pmat)/sum(pmat))
-plot(sort(ps), type = "l")
+avg_mc_acc_p(pmat, 90)
+avg_mc_acc_p(pmat, 300)
 
 
+# rankconv <- (apply(pmat, 2, rank) - 0.5)/nrow(pmat)
+# plot(density(as.numeric(rankconv), weights = as.numeric(pmat)/sum(pmat)))
+# ps <- sample(as.numeric(rankconv), 10000, replace = TRUE, prob = as.numeric(pmat)/sum(pmat))
+# plot(sort(ps), type = "l")
+
+ppmat <- empirical_p_dist(pmat, 30, 100)
+res <- res_mixtools(ppmat, 30)
+
+avg_mc_acc_p(pmat, 90)
+est_moment(res, 90)
+
+avg_mc_acc_p(pmat, 300)
+est_moment(res, 300)
--- extrapolation/mle_theory.R
+++ extrapolation/mle_theory.R
@@ -2,6 +2,8 @@
 ##  Maximum likelihood deconvolution
 ####
 
+library(mixtools)
+
 generate_sample <- function(ps, prob.ps, k, n) {
   samp.p <- sample(ps, n, TRUE, prob = prob.ps)
   obs <- rbinom(n, k, samp.p)
@@ -16,50 +18,57 @@ est_moment <- function(res, k) {
   sum(res$theta[, 2]^k * res$lambda)
 }
 
-k.comps <- 3 ## number of components of mixture
-ps <- rbeta(k.comps, 10, 1) ## Ps
-prob.ps <-  rbeta(k.comps, 1, 1) ## prob of ps
-prob.ps <- prob.ps/sum(prob.ps)
+res_mixtools <- function(ppmat, k) {
+  Ys <- as.numeric(ppmat)
+  Ymat <- cbind(k - Ys, Ys)
+  res <- multmixEM(Ymat, k)
+  res
+}
 
-k <- 20 # size of binom
-n <- 1000 # how many dudes you see = k * r
-Ys <- generate_sample(ps, prob.ps, k, n)
-Ymat <- cbind(k - Ys, Ys)
-hist(Ys/k)
-library(mixtools)
-res <- multmixEM(Ymat, k = 10, verb = TRUE)
-est_moment(res, 2)
-pk_moment(ps, prob.ps, 2)
-mean(sapply(Ys, binmom, k, 2))
-
-est_moment(res, 3)
-pk_moment(ps, prob.ps, 3)
-mean(sapply(Ys, binmom, k, 3))
-
-est_moment(res, 5)
-pk_moment(ps, prob.ps, 5)
-mean(sapply(Ys, binmom, k, 5))
-
-est_moment(res, 10)
-pk_moment(ps, prob.ps, 10)
-mean(sapply(Ys, binmom, k, 10))
-
-
-est_moment(res, 20)
-pk_moment(ps, prob.ps, 20)
-mean(sapply(Ys, binmom, k, 20))
-
-est_moment(res, 25)
-pk_moment(ps, prob.ps, 25)
-
-est_moment(res, 30)
-pk_moment(ps, prob.ps, 30)
-mean((Ys/k)^30)
-est_moment(res, 50)
-pk_moment(ps, prob.ps, 50)
-
-
-mean((Ys/k)^2)
-
-rbind(res$theta[, 2][order(-res$lambda)], sort(res$lambda, decreasing = TRUE))
-rbind(ps, prob.ps)
+# k.comps <- 3 ## number of components of mixture
+# ps <- rbeta(k.comps, 10, 1) ## Ps
+# prob.ps <-  rbeta(k.comps, 1, 1) ## prob of ps
+# prob.ps <- prob.ps/sum(prob.ps)
+# 
+# k <- 20 # size of binom
+# n <- 1000 # how many dudes you see = k * r
+# Ys <- generate_sample(ps, prob.ps, k, n)
+# Ymat <- cbind(k - Ys, Ys)
+# hist(Ys/k)
+# library(mixtools)
+# res <- multmixEM(Ymat, k = 10, verb = TRUE)
+# est_moment(res, 2)
+# pk_moment(ps, prob.ps, 2)
+# mean(sapply(Ys, binmom, k, 2))
+# 
+# est_moment(res, 3)
+# pk_moment(ps, prob.ps, 3)
+# mean(sapply(Ys, binmom, k, 3))
+# 
+# est_moment(res, 5)
+# pk_moment(ps, prob.ps, 5)
+# mean(sapply(Ys, binmom, k, 5))
+# 
+# est_moment(res, 10)
+# pk_moment(ps, prob.ps, 10)
+# mean(sapply(Ys, binmom, k, 10))
+# 
+# 
+# est_moment(res, 20)
+# pk_moment(ps, prob.ps, 20)
+# mean(sapply(Ys, binmom, k, 20))
+# 
+# est_moment(res, 25)
+# pk_moment(ps, prob.ps, 25)
+# 
+# est_moment(res, 30)
+# pk_moment(ps, prob.ps, 30)
+# mean((Ys/k)^30)
+# est_moment(res, 50)
+# pk_moment(ps, prob.ps, 50)
+# 
+# 
+# mean((Ys/k)^2)
+# 
+# rbind(res$theta[, 2][order(-res$lambda)], sort(res$lambda, decreasing = TRUE))
+# rbind(ps, prob.ps)
