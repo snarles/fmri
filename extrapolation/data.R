c0a9690107da8d8c9d802148165d9ecdf0b93708
snarles <snarles@stanford.edu>
2016-04-29 13:28:38 -0700

    get ready to make a plot...

--- extrapolation/data.R
+++ extrapolation/data.R
@@ -13,6 +13,17 @@ getUs <- function(pmat, ncl, ny) {
   Us
 }
 
+getYs <- function(pmat, ncl, ny) {
+  rankconv <- apply(pmat, 2, rank)
+  Us <- list()
+  for (i in 1:ncl) {
+    Us[[i]] <- rankconv[i, 1:ny + (ny) * (i-1)]  
+  }
+  Us <- do.call(c, Us)
+  Us
+}
+
+
 pmat <- read.table('/home/snarles/github/predict_test_error/lala.txt', header = FALSE)
 saveRDS(pmat, "extrapolation/cifar100.rds")
 cl <- rep(1:100, each = 100)
@@ -33,5 +44,15 @@ hist(getUs(pmat, 100, 100))
 
 library(lineId)
 (ihat <- Ihat_LI(1 - mean(sa), 30))
-1 - lineId::piK(sqrt(2 * ihat), 100)
+(fa_i <- 1 - lineId::piK(sqrt(2 * ihat), 100))
 
+### Try MLE/constrained MLE
+sub_us <- getYs(pmat[1:30, 1:3000], ncl = 30, ny = 100)
+mle_est <-  res_mixtools(sub_us, 30)
+pseq <- seq(0, 1, 1/300)
+k <- 30
+cm <- cons_mle_est(Ys, k, pseq, 0.01)
+(fa_m <- est_moment(mle_est, 100))
+(fa_c <- cm_est_moment(cm, 100))
+
+c(fa, fa_i, fa_m, fa_c)
