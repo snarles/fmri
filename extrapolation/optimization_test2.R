e1355f4782504467bcfaaa3d81b1c75f65fd009e
Charles Zheng <snarles@stanford.edu>
2016-05-12 17:01:56 -0700

    following vignette

--- extrapolation/optimization_test2.R
+++ extrapolation/optimization_test2.R
@@ -121,3 +121,36 @@ eval_grad_f <- function(x) {
 x0 <- c(-1.2, 1)
 opts = list("algorithm" = "NLOPT_LD_LBFGS", "xtol_rel" = 1.0e-8)
 res <- nloptr(x0 = x0, eval_f = eval_f, eval_grad_f = eval_grad_f, opts = opts)
+
+## Second example
+
+eval_f0 <- function(x, a, b) {
+  return( sqrt(x[2]) )
+}
+eval_grad_f0 <- function(x, a, b) {
+  return( c(0, .5/sqrt(x[2])) )
+}
+eval_g0 <- function(x, a, b) {
+  return( (a * x[1] + b)^3 - x[2] )
+}
+eval_jac_g0 <- function(x, a, b) {
+  return( rbind( c(3*a[1]*(a[1]*x[1] + b[1])^2, -1.0 ),
+                 c(3*a[2]*(a[2]*x[2] + b[2])^2, -1.0 ) ) )
+}
+a <- c(2, -1)
+b <- c(0, 1)
+res0 <- nloptr(x0 = c(1.234, 5.678),
+               eval_f = eval_f0,
+               eval_grad_f = eval_grad_f0,
+               lb = c(-Inf, 0),
+               ub = c(Inf, Inf),
+               eval_g_ineq = eval_g0,
+               eval_jac_g_ineq = eval_jac_g0, 
+               opts = list(algorithm = "NLOPT_LD_MMA",
+                           xtol_rel = 1.0e-8,
+                           print_level = 2,
+                           check_derivatives = TRUE,
+                           check_derivatives_print = "all"),
+               a = a, b = b)
+
+
