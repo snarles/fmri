ce7334976428aaa6e38f734d766863b507ff9ed3
Charles Zheng <snarles@stanford.edu>
2016-05-19 11:27:40 -0700

    results

--- extrapolation/run_simulation.R
+++ extrapolation/run_simulation.R
@@ -9,3 +9,34 @@ for (k in ks) {
   tabs <- c(tabs, list(tab))
 }
 
+saveRDS(tabs, file = "extrapolation/simres.rds")
+
+colnames(tab)
+
+cols <- c("black", "green", "red", "blue")
+WW <- 4
+HH <- 4
+
+pdf("extrapolation/sim_qda.pdf", width = WW, height = HH)
+method <- "qda"
+accs <- sapply(tabs, function(v) v[method, c(1, 3, 7, 8)])
+matplot(ks, t(accs), type = "l", lwd = 3, col = cols, lty = 1, ylim = c(0, 1))
+abline(accs["acc_sub", length(ks)], 0, lty = 2, lwd = 2)
+title("QDA")
+dev.off()
+
+pdf("extrapolation/sim_glmnet.pdf", width = WW, height = HH)
+method <- "glmnet"
+accs <- sapply(tabs, function(v) v[method, c(1, 3, 7, 8)])
+matplot(ks, t(accs), type = "l", lwd = 3, col = cols, lty = 1, ylim = c(0, 1))
+abline(accs["acc_sub", length(ks)], 0, lty = 2, lwd = 2)
+title("Logistic")
+dev.off()
+
+pdf("extrapolation/sim_nnet.pdf", width = WW, height = HH)
+method <- "nnet"
+accs <- sapply(tabs, function(v) v[method, c(1, 3, 7, 8)])
+matplot(ks, t(accs), type = "l", lwd = 3, col = cols, lty = 1, ylim = c(0, 1))
+abline(accs["acc_sub", length(ks)], 0, lty = 2, lwd = 2)
+title("Neural Net")
+dev.off()
