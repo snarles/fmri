0d38c66aa8e1cf56243ff6cb506de237f9a5b6e0
Charles Zheng <snarles@stanford.edu>
2016-09-08 19:41:17 -0700

    turn into source so we can use it

--- mi_vs_aba/alpha_vs_beta.R
+++ mi_vs_aba/alpha_vs_beta.R
@@ -7,7 +7,10 @@
 # sum(qs * seq(0, 1, length.out = length(qs))^(k-1))/length(qs)
 # mean(sapply(1:1000, function(i) max(sample(qs, k, TRUE))))/k
 
-get_q_ints <- function(qs) {
+k <- 6
+reso <- 1000
+
+get_q_ints <- function(qs, k) {
   qs <- sort(qs)
   c(sum(qs), sum(qs * log(qs)),
     sum(qs * seq(0, 1, length.out = length(qs))^(k-1)))/length(qs)
@@ -17,25 +20,22 @@ normalize_qs <- function(qs) {
   sort(qs)/sum(qs) * length(qs)
 }
 
-qs <- normalize_qs(rbeta(100, 1, 5))
-get_q_ints(qs)
-
 qs_par <- function(d, l.out = 100) {
   xs <- seq(0, 1, length.out = l.out)
   normalize_qs(exp( - d * xs))
 }
 
-pts <- sapply(1:1000, function(i) {
-  qs <- rbeta(100, runif(1) * 5, runif(1) * 5)
-  qs <- normalize_qs(qs)
-  get_q_ints(qs)
-})
-
-pts2 <- sapply(10 * (1:1000/1000), function(x) {
-  qs <- qs_par(x)
-  get_q_ints(qs)
-})
-
-plot(t(pts)[, -1], pch= ".")
-lines(t(pts2)[, -1], col = "red")
+# pts <- sapply(1:10000, function(i) {
+#   qs <- rbeta(reso, runif(1) * 5, runif(1) * 5)
+#   qs <- normalize_qs(qs)
+#   get_q_ints(qs)
+# })
+# 
+# pts2 <- sapply(1000 * (1:10000/10000), function(x) {
+#   qs <- qs_par(x, reso)
+#   get_q_ints(qs)
+# })
+# 
+# plot(t(pts2)[, -1], pch= ".")
+# lines(t(pts2)[, -1], col = "red")
 
