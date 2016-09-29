0e6dfa9ceb3d765928c2c911cdba397adf22fa82
snarles <snarles@stanford.edu>
2016-09-28 18:16:50 -0700

    results1

--- mi_vs_aba/empirical_kmax_vs_varkmax_vec.R
+++ mi_vs_aba/empirical_kmax_vs_varkmax_vec.R
@@ -5,7 +5,7 @@ compute_moments <- function(d, alpha, k, mc.reps = 1e4) {
   maxstat <- sapply(1:mc.reps, function(i) {
     aa <- matrix(rgamma(d * k, alpha), k, d)
     rs <- rowSums(aa)
-    aa[rs == 0, ] <- 1
+    aa[cbind(which(rs == 0), sample(d, sum(rs == 0), replace = TRUE))] <- 1
     rs <- rowSums(aa)
     aa <- aa/rowSums(aa)
     lala <- apply(aa, 2, max)/k
@@ -33,5 +33,12 @@ compute_moments(3, 4e-3, 3) # 0.7025812 0.1868771
 compute_moments(3, 2e-3, 3) # 0.7054425 0.1810342
 
 ## d = 4
-compute_moments(4, 5e-3, 3)
+compute_moments(4, 0.5, 3)  # 0.6276666 0.0934213
+compute_moments(4, 1e-2, 3) # 0.7711520 0.1874884
+compute_moments(4, 5e-3, 3) # 0.7693966 0.1897850
+
+## d > 4
+compute_moments(5, 5e-3, 3) # 0.8136139 0.1841528
+compute_moments(6, 5e-3, 3) # 0.8433355 0.1773458
+
 
