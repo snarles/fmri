e5560d88b79ca1b9a99e6dcaee66515a01293110
snarles <snarles@stanford.edu>
2016-10-04 23:06:49 -0700

    missing files

---
+++ mi_vs_aba/alpha_vs_beta_illus2.R
@@ -0,0 +1,36 @@
+## get the curve
+
+k <- 5
+reso <- 1000
+
+pts <- sapply(1:10000, function(i) {
+  qs <- rbeta(reso, runif(1) * 10, runif(1) * 10)
+  qs <- normalize_qs(qs)
+  get_q_ints(qs, k)
+})
+
+pts2 <- sapply(1000 * (1:10000/10000), function(x) {
+  qs <- qs_par(x, k, reso)
+  get_q_ints(qs, k)
+})
+
+# plot(t(pts)[, -1], pch= ".")
+# lines(t(pts2)[, -1], col = "red")
+# 
+# 
+# 
+# 
+# plot(qs_par(d = 2, k = 3), type = "l")
+# plot(qs_par(d = 2, k = 4), type = "l")
+# plot(qs_par(d = 2, k = 4), type = "l")
+# plot(qs_par(d = 10, k = 4), type = "l")
+# 
+
+ppts <- t(pts2)[, -1]
+ppts <- ppts[complete.cases(ppts), ]
+plot(ppts[, 1], ppts[, 2], type = "l", ylim = c(0, 1), xlab = "CI", ylab = "CA")
+polygon(rbind(ppts, c(max(ppts[, 1], na.rm = TRUE), 1/k), c(0, 1/k)), col = "grey", border = NA)
+lines(ppts[, 1], ppts[, 2])
+lines(c(0, max(ppts[, 1])), c(1/k, 1/k))
+title("k = 5")
+text(3.5, 0.5, "S", cex = 3)
