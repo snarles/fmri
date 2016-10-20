25743f3a2fc0b85bfcc8df3256142392d9428dc9
snarles <snarles@stanford.edu>
2016-10-20 13:46:26 -0700

    corrected formula

--- mi_vs_aba/large_k_limit.R
+++ mi_vs_aba/large_k_limit.R
@@ -6,8 +6,18 @@ ts <- seq(0, 1, by = delt)
 ks <- 1:200
 bts <- exp(seq(-2, 3, by = 0.01))
 #curves <- sapply(ks, function(k) delt * rowSums(exp(bts %*% t(ts)^(k-1))))
-curves <- sapply(ks, function(k) (k-1) * delt * rowSums(exp(bts %*% t(ts)^(k-1))))
+curves <- sapply(ks, function(k) {
+  (k-1) * (delt * rowSums(exp(bts %*% t(ts)^(k-1))) - 1)
+})
 
-limit_curve <- colSums((t(exp(bts %*% t(ts))) * delt/ts)[-1, ])
+is <- 2:100
+limit_curve <- sapply(bts, function(bt) {
+  sum(exp(is * log(bt) - lgamma(is + 1) - log(is)))
+})
 matplot(bts, curves[, 5 * 1:10], type = "l")
-lines(bts, limit_curve, type = "l", lwd = 2)
+
+plot(ks, curves[4, ], type = "l")
+
+plot(bts, curves[, 200], type = "l")
+lines(bts, limit_curve, col = "red")
+
