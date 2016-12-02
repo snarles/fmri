c32dc312351e218c3e51c84b8daba61528c67378
snarles <snarles@stanford.edu>
2016-12-02 15:16:47 -0800

    sim results general spline

--- extrapolation/ku_regression_sim.R
+++ extrapolation/ku_regression_sim.R
@@ -44,8 +44,11 @@ lines(seq(0, 1, 0.01), ku, col = "blue")
 
 avrisk(k, bt)
 cvec
+avrisk(5, bt)
+
 avrisk(30, bt)
-plot(2:300, avrisk(2:300, bt), type = "l")
+avrisk(100, bt)
+plot(20:300, avrisk(20:300, bt), type = "l")
 
 
 ####
@@ -76,7 +79,7 @@ bt <- lsei(A = Ws * get_avrisk_mat(2:k, d), B = Ws * get_sub_errs(pmat, true_ys,
 ks <- 20:k
 avr_w <- get_avrisk_mat(ks, d)
 avr <- get_sub_errs(pmat, true_ys, ks)
-(bt <- solve(t(avr_w) %*% avr_w, t(avr_w) %*% avr))
+(bt <- pinv(avr_w) %*% avr)
 sum(bt)
 
 ku <- get_vande(d = d) %*% bt
@@ -85,4 +88,37 @@ plot(kmat[, 2], cvec, ylim = c(0, 1), type = "l")
 lines(kmat[, 2], yhat, col = "red")
 lines(seq(0, 1, 0.01), ku, col = "blue", type = "l")
 
-plot(2:300, avrisk(2:300, bt), type = "l")
+plot(20:300, avrisk(20:300, bt), type = "l")
+
+
+#####
+##  Spline
+#####
+
+nsplines <- 20
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- knts[-c(1, nsplines + 2)]
+ffs <- lapply(knts, spline1_maker)
+
+Kmax <- 500
+
+ks <- 20:k
+MM <- make_moment_mat(ffs, 1:Kmax, res = 1e5)
+xmat <- MM[ks, ]
+avr <- get_sub_errs(pmat, true_ys, ks)
+
+#bt <- pinv(xmat) %*% avr
+bt <- nnls(xmat, avr)$X
+sum(bt)
+plot(MM %*% bt, type = "l")
+
+ts <- seq(0, 1, 0.01)
+xs <- sapply(ffs, function(ff) ff(ts))
+ku <- as.numeric(xs %*% bt)
+
+plot(kmat[, 2], cvec, ylim = c(0, 1), type = "l")
+lines(seq(0, 1, 0.01), ku, col = "blue", type = "l")
+
+
+
+
