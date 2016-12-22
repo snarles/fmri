eba9e2ff60f90dd0e459a9233dcff18593c0ef0e
snarles <snarles@stanford.edu>
2016-12-22 11:18:08 -0800

    averaging the curves fixed the problem?

--- info_theory_sims/sim5.R
+++ info_theory_sims/sim5.R
@@ -25,11 +25,26 @@ Bhat <- solve(t(Xtr) %*% Xtr, t(Xtr) %*% Ytr)
 Yhat <- Xte %*% Bhat
 dmat <- pdist2(Yhat, Yte)
 acs <- 1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+acss <- sapply(1:30, function(i) {
+  Xte <- randn(nte, p)
+  Yte <- Xte %*% B + sigma * randn(nte, q)
+  Yhat <- Xte %*% Bhat
+  dmat <- pdist2(Yhat, Yte)
+  1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+})
+acs_mu <- rowMeans(acss)
 plot(acs, type = "l")
-k_sub <- 20
+k_sub <- 50
 (I_implied <- fit_I_to_curve(acs[1:k_sub], wt_exp = 0))
+(I_implied2 <- fit_I_to_curve(acs_mu[1:k_sub], wt_exp = 0, nits = 20, i_max = 4))
+
 acs_hat <- acs_curve(I_implied, 1:nte)
+acs_hat2 <- acs_curve(I_implied2, 1:nte)
+
 plot(1:nte, acs, type = "l"); lines(acs_hat, col = "red"); abline(v = k_sub)
+lines(acs_mu, col = "blue")
+lines(acs_hat2, col = "green")
+
 
 
 ## high-dim
@@ -50,11 +65,65 @@ Bhat <- solve(t(Xtr) %*% Xtr, t(Xtr) %*% Ytr)
 Yhat <- Xte %*% Bhat
 dmat <- pdist2(Yhat, Yte)
 acs <- 1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+acss <- sapply(1:30, function(i) {
+  Xte <- randn(nte, p)
+  Yte <- Xte %*% B + sigma * randn(nte, q)
+  Yhat <- Xte %*% Bhat
+  dmat <- pdist2(Yhat, Yte)
+  1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+})
+acs_mu <- rowMeans(acss)
 plot(acs, type = "l")
-k_sub <- 20
+k_sub <- 50
 (I_implied <- fit_I_to_curve(acs[1:k_sub], wt_exp = 0))
+(I_implied2 <- fit_I_to_curve(acs_mu[1:k_sub], wt_exp = 0))
+
 acs_hat <- acs_curve(I_implied, 1:nte)
+acs_hat2 <- acs_curve(I_implied2, 1:nte)
+
 plot(1:nte, acs, type = "l"); lines(acs_hat, col = "red"); abline(v = k_sub)
+lines(acs_mu, col = "blue")
+lines(acs_hat2, col = "green")
+
+## bi cluster
+
+ntr <- 2000
+nte <- 100
+p <- 30
+q <- 50
+sigma <- 10
+B <- randn(p, q)
+
+Xtr <- randn(ntr, p)
+Xtr[, 1] <- Xtr[, 1] + 15 * (rbinom(ntr, 1, .5) - .5)
+Ytr <- Xtr %*% B + sigma * randn(ntr, q)
+Xte <- randn(nte, p)
+Xte[, 1] <- Xte[, 1] + 15 * (rbinom(nte, 1, .5) - .5)
+Yte <- Xte %*% B + sigma * randn(nte, q)
+
+Bhat <- solve(t(Xtr) %*% Xtr, t(Xtr) %*% Ytr)
+Yhat <- Xte %*% Bhat
+dmat <- pdist2(Yhat, Yte)
+acs <- 1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+acss <- sapply(1:30, function(i) {
+  Xte <- randn(nte, p)
+  Yte <- Xte %*% B + sigma * randn(nte, q)
+  Yhat <- Xte %*% Bhat
+  dmat <- pdist2(Yhat, Yte)
+  1-sapply(1:nte, function(i) resample_misclassification(-dmat, 1:nte, i))
+})
+acs_mu <- rowMeans(acss)
+plot(acs, type = "l")
+k_sub <- 50
+(I_implied <- fit_I_to_curve(acs[1:k_sub], wt_exp = 0))
+(I_implied2 <- fit_I_to_curve(acs_mu[1:k_sub], wt_exp = 0))
+
+acs_hat <- acs_curve(I_implied, 1:nte)
+acs_hat2 <- acs_curve(I_implied2, 1:nte)
+
+plot(1:nte, acs, type = "l"); lines(acs_hat, col = "red"); abline(v = k_sub)
+lines(acs_mu, col = "blue")
+lines(acs_hat2, col = "green")
 
 
 
