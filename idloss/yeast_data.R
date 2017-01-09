5689154739e7d63377374036d3d0ebd8cad76a42
snarles <snarles@stanford.edu>
2017-01-09 14:57:49 -0800

    extract cell-cycle related genes

--- idloss/yeast_data.R
+++ idloss/yeast_data.R
@@ -1,4 +1,8 @@
 
+###
+## Reading yeast data
+###
+
 dat <- read.delim("data2/yeast_cell_cycle.txt", row.names = 1)
 #View(dat)
 plot(dat[, 1])
@@ -25,7 +29,7 @@ cdc_dat_s <- scale(cdc_dat)
 
 dim(cdc_dat_s)
 
-ncl <- 10
+ncl <- 100
 res <- kmeans(t(cdc_dat_s), ncl, iter.max = 100, nstart = 10)
 table(res$cluster)
 matplot(cdc_dat_s[, res$cluster == 1], type = "l")
@@ -42,4 +46,30 @@ inds <- sapply(1:ncl, function(i) which.max(varis * (res$cluster == i)))
 cdc_dat_r <- cdc_dat_s[, inds]
 cor(cdc_dat_r[, 1:10], method = "spearman")
 
+res_svd <- svd(cdc_dat_r)
+plot(res_svd$d)
+plot(res_svd$u[, 1])
+plot(res_svd$u[, 2])
 
+
+###
+## Obtaining cell-cycle related genes
+###
+
+library(readxl)
+annot <- read_excel("data2/CellCycle98.xls", skip = 3)
+View(annot)
+class(annot)
+colnames(annot)
+sort(table(annot$Process), decreasing = TRUE)[1:10]
+
+## matching the rownames to the processes
+
+colnames(cdc_dat)[1:10]
+annot$ORF[1:20]
+
+annots <- character(ncol(cdc_dat))
+m_inds <- match(colnames(cdc_dat), annot$ORF)
+annots[!is.na(m_inds)] <- annot$Process[m_inds[!is.na(m_inds)]]
+annots[1:10]
+names(annots) <- colnames(cdc_dat)
