8af1d4ab623fd1bbf4715fcab015df6cad5bfd3f
snarles <snarles@stanford.edu>
2017-02-07 13:24:18 -0800

    checked correctness of code

--- extrapolation/simulation_models.R
+++ extrapolation/simulation_models.R
@@ -25,31 +25,39 @@ simulate_qda <- function(
       x <- randn(wishart_par * p, p)
       (t(x) %*% x)/(wishart_par * p)
     })
-    xerrs <- lapply(1:k, function(ind) {
-      mvrnorm(r_train + r_test, rep(0, p), Sigmas[[ind]])
+    xtt <- lapply(1:k, function(ind) {
+      mvrnorm(r_train + r_test, mus[ind, ], Sigmas[[ind]])
     })
   } else {
     Sigmas <- rep(list(eye(p)), k)
-    xerrs <- lapply(1:k, function(ind) randn(r_train + r_test, p))
+    xtt <- lapply(1:k, function(ind) {
+      repmat(mus[ind, ], (r_train + r_test), 1) + 
+        randn(r_train + r_test, p)
+    })
   }
+  xtrains <- lapply(xtt, function(x) x[(1:r_train), , drop = FALSE])
+  xtests <- lapply(xtt, function(x) x[-(1:r_train), , drop = FALSE])
   ## Estimate the parameters from data
   if (bayes) {
-    mu_hats <- mus
-    Sigma_hats <- Sigmas
+    return(compute_qda_posterior(xtrains, xtests, mus, Sigmas))
   } else {
-    mu_errs <- lapply(xerrs, function(x) colMeans(x[(1:r_train), ])) %>%
-      do.call(what = rbind)
-    mu_hats <- mus + mu_errs
-    Sigma_hats <- lapply(xerrs, function(x) cov(x[(1:r_train), ]))
+    return(compute_qda_posterior(xtrains, xtests))
   }
+}
+
+compute_qda_posterior <- function(xtrains, xtests, 
+                                  mu_hats = NULL, Sigma_hats = NULL) {
+  k <- length(xtrains)
+  if (is.null(mu_hats)) {
+    mu_hats <- lapply(xtrains, colMeans) %>% do.call(what = rbind)
+    Sigma_hats <- lapply(xtrains, cov)
+  }
+  i_chosen <- sapply(1:length(xtests), 
+                     function(ind) rep(ind, nrow(xtests[[ind]])))
+  xtest <- do.call(rbind, xtests)
   iSigma_hats <- lapply(Sigma_hats, solve)
   Sigma_dets <- sapply(Sigma_hats, det)
   iSigma_stack <- lapply(iSigma_hats, as.numeric) %>% do.call(what = rbind)
-  xtest <- lapply(1:k, function(ind) {
-    repmat(mus[ind, ], r_test, 1) + xerrs[[ind]][-(1:r_train), ]
-  }) %>%
-    do.call(what = rbind)
-  i_chosen <- rep(1:k, each = r_test)
   ## Compute the matrix of margins, dimension is
   ##  (est_class k * true_class k * repeat r_test) x p 
   diffs <- repmat(xtest, k, 1) - (mu_hats %x% ones(nrow(xtest), 1))
@@ -61,7 +69,7 @@ simulate_qda <- function(
   ## check correctness:
   i_ind <- sample(k, 1);j_ind <- sample(k, 1);r_ind <- sample(r_test, 1)
   rbind(diffs[((j_ind - 1) * k + (i_ind - 1))*r_test + r_ind, ],
-    xtest[i_ind, ] - mu_hats[j_ind, ])
+        xtest[i_ind, ] - mu_hats[j_ind, ])
   c(ssquares[(i_ind-1)*r_test + r_ind, j_ind],
     t(xtest[i_ind, ] - mu_hats[j_ind, ]) %*% iSigma_hats[[j_ind]] %*%
       (xtest[i_ind, ] - mu_hats[j_ind, ]))
@@ -70,8 +78,18 @@ simulate_qda <- function(
   list(plikes = plikes, i_chosen = i_chosen)
 }
 
-# res <- simulate_qda(snr = 20, wishart_par = 1, bayes = TRUE)
-# # res <- simulate_qda(snr = 20, wishart_par = Inf, bayes = TRUE)
+## Compare to built-in qda
+# res <- compute_qda_posterior(xtrains, xtests)
+# do.call(topk_scores, res)
+# grp <- sapply(1:length(xtrains),
+#               function(ind) rep(ind, nrow(xtrains[[ind]])))
+# res_qda <- qda(x = do.call(rbind, xtrains), grouping = grp)
+# pred_qda <- predict(res_qda, do.call(rbind, xtests))$posterior
+# topk_scores(pred_qda, res$i_chosen)
+
+## Compare to MC results
+# # res <- simulate_qda(snr = 20, wishart_par = 1, bayes = TRUE)
+# res <- simulate_qda(snr = 20, wishart_par = Inf, bayes = TRUE)
 # do.call(lineId::topk_scores, res)
 # source("approximation/gaussian_lc_source.R")
 # 1 - mc(20 * eye(2), 100)
