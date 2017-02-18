1a50a3fed14c3f0a200a5d4ec11e4ddc726f2670
snarles <snarles@stanford.edu>
2017-02-18 15:03:26 -0800

    great classification rates!!

---
+++ facerec/load_data.R
@@ -0,0 +1,33 @@
+library(class)
+library(pracma)
+
+x <- read.csv("att_faces_aligned/reps.csv", header = FALSE)
+x <- as.matrix(x)
+View(x)
+dim(x)
+labs <- read.csv("att_faces_aligned/labels.csv", header = FALSE, stringsAsFactors = FALSE)
+dim(labs)
+help(substr)
+labs[, 2] <- sapply(labs[, 2], substring, 19)
+y <- labs[, 1]
+rownames(x) <- paste0(y, "_", (1:length(y)) %% 11)
+
+floor(pdist(x[y <= 2, ]) * 10)
+
+## distance matrix
+image(pdist(x[y <= 10, ]))
+
+## built test set
+test_inds <- sapply(1:40, function(x) sample(which(labs[, 1]==x), 1))
+x.te <- x[test_inds, ]
+x.tr <- x[-test_inds, ]
+y.te <- y[test_inds]
+y.tr <- y[-test_inds]
+
+## success of knn???
+knn_errs <- numeric()
+for (k in 1:5) {
+  res <- knn(train = x.tr, test = x.te, cl = y.tr, k = k)
+  knn_errs[paste0("k_", k)] <- mean(res!=y.te)
+}
+knn_errs
