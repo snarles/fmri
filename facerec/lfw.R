f8f0c14a1492c4ec3cf47421dff77e6f438996d9
snarles <snarles@stanford.edu>
2017-02-18 16:55:25 -0800

    faces in wild extrapolation

--- facerec/lfw.R
+++ facerec/lfw.R
@@ -8,5 +8,33 @@ labs <- read.csv("facerec/labels.csv", header = FALSE, stringsAsFactors = FALSE)
 dim(labs)
 labs[, 2] <- sapply(labs[, 2], substring, 19)
 y <- labs[, 1]
-rownames(x) <- paste0(y, "_", (1:length(y)) %% 11)
+rownames(x) <- labs[, 2]
 save(x, y, file = "facerec/lfw.RData")
+
+## get 2-repeat data
+yc <- table(y)
+y2s <- as.numeric(names(yc)[yc > 1])
+head(y2s)
+filt <- y %in% y2s
+xf <- x[filt, ]; yf <- y[filt]
+
+split_inds <- sapply(unique(yf), function(x) sample(which(yf==x), 2))
+length(split_inds)
+split_ind_m <- matrix(split_inds, nrow = 2)
+
+dim(split_ind_m)
+
+nsub <- 100
+clsub <- sample(ncol(split_ind_m), nsub)
+tr_inds <- split_ind_m[1, clsub]
+te_inds <- split_ind_m[2, clsub]
+xtr <- xf[tr_inds, ]
+xte <- xf[te_inds, ]
+ytr <- yf[tr_inds]
+yte <- yf[te_inds]
+
+res <- knn(train = xtr, test = xte, cl = ytr, k = 1)
+(knn_err <- mean(res!=yte))
+
+
+
