bc962b6517b7c14a9a132f0c9b660e2929c25790
snarles <snarles@stanford.edu>
2017-02-21 15:32:34 -0800

    more facerec results

--- facerec/lfw.R
+++ facerec/lfw.R
@@ -81,3 +81,52 @@ abline(v = nsub, lty = 2, col = "red")
 legend(500, 0.3, c("sub400", "estimate", "full1672"), col = c("black", "red", "blue"), 
        lwd = c(2, 1, 1), cex = 1.5)
 dev.off()
+
+## multiple repeats
+
+draw_sub_accs <- function(nsub) {
+  clsub <- sample(ncol(split_ind_m), nsub)
+  tr_inds <- split_ind_m[1, clsub]
+  te_inds <- split_ind_m[2, clsub]
+  xtr <- xf[tr_inds, ]
+  xte <- xf[te_inds, ]
+  ytr <- yf[tr_inds]
+  yte <- yf[te_inds]
+  liks <- -pdist2(xte, xtr)
+  accs <- 1 - resample_misclassification(liks, 1:nsub, 1:nsub)
+  nsplines <- 10000
+  acs <- 1 - accs
+  knts <- seq(0, 1, length.out = nsplines + 2)
+  knts <- knts[-c(1, nsplines + 2)]
+  MM <- spline1_moments(knts, 1:ncol(split_ind_m))
+  xmat <- spline1_moments(knts, 1:nsub)
+  bt <- nnls::nnls(xmat, acs)$x
+  xs <- seq(0, 1, 0.01)
+  acs_hat <- pmax(1 - as.numeric(MM %*% bt), 0)
+  acs_hat
+}
+
+nreps <- 20
+nsubs <- c(50, 100, 200, 400, 800)
+#nsub <- 400
+
+for (nsub in nsubs) {
+  pdf(paste0("facerec/sub_", nsub, ".pdf"))
+  plot(accs_full, type = "l", ylim = c(0, 1), xlab = "faces", ylab = "accuracy",
+       main = paste(nsub), cex.lab = 1.5, col = "blue")
+  abline(v = nsub, lty = 2)
+  for (i in 1:nreps) lines(draw_sub_accs(nsub), col = "red")
+  dev.off()
+}
+
+nreps <- 50
+accs_final <- list()
+for (nsub in nsubs) {
+  accs <- sapply(1:nreps, function(i) min(draw_sub_accs(nsub)))
+  accs_final[[paste(nsub)]] <- accs
+}
+
+boxplot(accs_final, ylim = c(0, 1))
+abline(h = min(accs_full), col = "blue")
+
+saveRDS(accs_final, file = "lfw_sub_preds.rds")
