deb690e60c6e96244d7e50f49aa45a8d4034be2f
Charles Zheng <snarles@stanford.edu>
2017-07-04 11:07:27 +0300

    simulation vs two-par classification curve model

--- approximation/gaussian_identity_finsam.R
+++ approximation/gaussian_identity_finsam.R
@@ -2,9 +2,10 @@ library(pracma)
 library(MASS)
 library(class)
 library(parallel)
-library(reginference) ## see github.com/snarles/misc
+#library(reginference) ## see github.com/snarles/misc
 library(lineId) ## use devtools::install('lineId')
 #source("approximation/extreme_value.R")
+
 
 
 binmom <- function(succ, tot, k) {
@@ -53,29 +54,6 @@ mc_ident_fs_curve <- function(p, sigma2, sigma2_tr, K, mc.reps = 100) {
   rowMeans(mcs)
 }
 
-### Which is faster?
-# 
-# p <- 3
-# sigma2 <- 0.1
-# sigma2_tr <- 0.1
-# K <- 500
-# mc.reps <- 10
-# 
-# t1 <- proc.time()
-# res1 <- numeric(K)
-# for (k in 2:K) res1[k] <- mc_ident_fs(p, sigma2, sigma2_tr, k, mc.reps)
-# proc.time() - t1
-# 
-# t1 <- proc.time()
-# res2 <- mc_ident_fs_curve(p, sigma2, sigma2_tr, K, mc.reps)
-# proc.time() - t1
-# 
-# plot(res1); points(res2, col= "red")
-
-
-
-### OLD CODE vvvvv
-
 ## uses noncentral chi squared
 ## same Evalue as mc_ident
 mc_ident2 <- function(p, sigma2, K, mc.reps = 1000) {
@@ -90,159 +68,84 @@ mc_ident2 <- function(p, sigma2, K, mc.reps = 1000) {
   mean(mcs)
 }
 
-## actually generates a gaussian instead of a chi-squared
-rchisq_g <- function(n, df, ncp = 0) {
-  mu <- df + ncp
-  vv <- 2 * df + 4 * ncp
-  sqrt(vv) * rnorm(n) + mu
-}
-
-meanexp <- function(v) {
-  vm <- max(v)
-  mean(exp(v - vm)) * exp(vm)
-}
-
-## large-sigma approximation
-mc_indent3 <- function(p, sigma2, K, mc.reps = 1e4) {
-  cc <- p/sigma2
-  samp <- qnorm(((1:mc.reps) - 0.5)/mc.reps)  
-  1 - meanexp((K-1) * log(1 - pnorm(samp - sqrt(cc))))
-}
-
-## large-sigma2 approximation (if sigma2 > p)
-mc_ident3 <- function(p, sigma2, K, mc.reps = 1000) {
-  alpha <- sigma2/(1 + sigma2)
-  mcs <- sapply(1:mc.reps,
-                function(i) {
-                  y2 <- (1 + sigma2) * rchisq_g(1, df = p)
-                  if (y2 > 0) {
-                    d1 <- alpha * rchisq_g(1, df = p, ncp = alpha * y2)
-                    ds <- rchisq_g(K - 1, df = p, ncp = y2)
-                    return(min(ds) < d1)
-                  } else {
-                    return(1 - (1/K)) ## default in case of error
-                  }
-                })
-  mean(mcs)
-}
-
-min_chisq_approx <- function(K, df, ncp, naive = FALSE, nits = 20,
-                             pchisq_f = pchisq, prob = 1/2, verbose = TRUE) {
-  if (naive) {
-    print(min(rchisq(K, df, ncp)))
-  }
-  #z <- (ncp - df)/sqrt(df)
-  #med_loc2(df, z, K, ux=df + ncp, nits=nits)
-  ux <- df + ncp; lx <- 0
-  const <- log(prob)
-  for (i in 1:nits) {
-    xc <- (ux + lx)/2
-    suppressWarnings(pp <- pchisq_f(xc, df, ncp))
-    if (is.na(pp)) {
-      if (verbose) {paste("pchisq(", xc, ",", df, ",", ncp, ") error")}
-      ux <- xc
-    } else {
-      val <- K * log_1_plus(-pp)
-      if (is.na(val) && verbose) {
-        print(paste("xc = ", xc, ";df=", df, ";ncp=", ncp, ";pp=", pp, ";K=", K))
-      }
-      if (val > const) {
-        lx <- xc
-      } else {
-        ux <- xc
-      }     
-    }
-    #print(xc)
-  }
-  xc
-}
-
-## use exact = TRUE to sample from min dist
-## computes theoretical median of min
-mc_ident4 <- function(p, sigma2, K, mc.reps = 1000, nits = 20,
-                      exact = FALSE, use.qchisq = FALSE) {
-  alpha <- sigma2/(1 + sigma2)
-  mcs <- sapply(1:mc.reps,
-                function(i) {
-                  y2 <- (1 + sigma2) * rchisq(1, df = p)
-                  d1 <- alpha * rchisq(1, df = p, ncp = alpha * y2)
-                  prob <- 1/2
-                  if (exact == TRUE) prob <- runif(1)
-                  if (use.qchisq) {
-                    prob2 <- 1-(1-prob)^(1/(K-1))
-                    ds <- qchisq(prob2, p, y2)
-                  } else {
-                    ds <- min_chisq_approx(K - 1, p, y2, nits = nits, prob = prob)                    
-                  }
-                  (ds < d1)
-                })
-  mean(mcs)
-}
-
-## nonrandom -- computes theoretical median of min and uses cdf
-## same Evalue as mc_ident4
-mc_ident5 <- function(p, sigma2, K, mc.reps = 1000, nits = 20,
-                      pchisq_f = pchisq) {
-  alpha <- sigma2/(1 + sigma2)
-  quants <- (1:mc.reps - .5)/mc.reps
-  y2s <- (1 + sigma2) * qchisq(quants, df = p)
-  mcs <- sapply(y2s,
-                function(y2) {
-                  ds <- min_chisq_approx(K - 1, p, y2, nits = nits,
-                                         pchisq_f = pchisq_f)
-                  1 - pchisq_f(ds/alpha, p, alpha * y2) 
-                })
-  mean(mcs)
-}
-
-####
-##  see large_deviations_source to use pchisq_laplace
-####
-
-
-
-
-# mean(rchisq(1e3, 3, 2.2))
-# mean(rchisq_g(1e3, 3, 2.2))
-# var(rchisq(1e3, 3, 2.2))
-# var(rchisq_g(1e3, 3, 2.2))
+# ### Which is faster?
 # 
-# ## Gaussian approximation is unusably bad
-# 
-# mc_ident(10, 3, 10, 1e4)
-# mc_ident2(10, 3, 10, 1e4)
-# mc_ident4(10, 3, 10, 1e4)
-# 
-# mc_ident( 20, 1, 1e3, 1e3)
-# mc_ident2(20, 1, 1e3, 1e3)
-# mc_ident4(20, 1, 1e3, 1e3)
-# 
-# 
-# df <- 40; sigma2 <- 2; L <- 1e5; nits <- 100
-# 
-# tims <- list(); res <- numeric()
-# t1 <- proc.time()
-# (res[1] <- mc_ident( df, sigma2, L, 1))
-# (tims[[1]] <- proc.time() - t1)
+# p <- 3
+# sigma2 <- 0.1
+# sigma2_tr <- 0.1
+# K <- 500
+# mc.reps <- 100
 # 
 # t1 <- proc.time()
-# (res[2] <- mc_ident2(df, sigma2, L, nits))
-# (tims[[2]] <- proc.time() - t1)
-# 
-# t1 <- proc.time()
-# (res[3] <- mc_ident4(df, sigma2, L, nits))
-# (tims[[3]] <- proc.time() - t1)
-# 
-# res
-# tims
-# 
-# 
-# ## push the dimensionality and # of classes
-# 
-# min_chisq_approx(1e5, 20, 20, TRUE)
-# min_chisq_approx(1e6, 30, 30, TRUE)
-# t1 <- proc.time()
-# min_chisq_approx(1e8, 40, 40, TRUE); gc()
+# res1 <- numeric(K)
+# for (k in 2:K) res1[k] <- mc_ident_fs(p, sigma2, sigma2_tr, k, mc.reps)
 # proc.time() - t1
+# 
+# t1 <- proc.time()
+# res2 <- mc_ident_fs_curve(p, sigma2, sigma2_tr, K, mc.reps)
+# proc.time() - t1
+# 
+# plot(res1); points(res2, col= "red")
+# 
+# ### Which is faster? 2
+# 
+# t1 <- proc.time()
+# res1 <- numeric(K)
+# for (k in 2:K) res1[k] <- mc_ident2(p, sigma2, k, mc.reps)
+# proc.time() - t1
+# 
+# 
+# t1 <- proc.time()
+# res2 <- mc_ident_fs_curve(p, sigma2, 0, K, mc.reps)
+# proc.time() - t1
+# 
+# res1 <- numeric(K)
+# for (k in 2:K) res1[k] <- mc_ident2(p, sigma2, k, mc.reps * 10)
+# 
+# plot(res1); points(res2, col= "red")
 
+## fitting the piK function
 
+mugrid <- seq(0.1, 5, 0.1)
+sigma2grid <- seq(0.1, 5, 0.1)
+length(mugrid) * length(sigma2grid)
+
+parmat <- cbind(mu = rep(mugrid, each = length(sigma2grid)),
+                sigma2 = rep(sigma2grid, length(mugrid)))
+
+nrow(parmat)
+
+t1 <- proc.time()
+par_res <- mclapply(1:nrow(parmat), function(i) {
+  mu <- parmat[i, 1]
+  sigma2 <- parmat[i, 2]
+  1-piK(K = 1:250, mc.reps = 1000, mu, sigma2)
+}, mc.cores = 2)
+proc.time() - t1
+
+precomputed_curves <- do.call(cbind, par_res)
+save(parmat, precomputed_curves, file = "approximation/ident_curve_precomp.rda")
+
+K <- 250
+k_sub <- 125
+p <- 3
+sigma2 <- 0.1
+mc.reps <- 100
+acs <- 1-mc_ident_fs_curve(p, sigma2, 0, K, mc.reps)
+
+## find best 2-parameter fit
+dd <- colSums((precomputed_curves - acs)[1:k_sub, ]^2)
+(pars <- parmat[which.min(dd), ])
+acs_hat_2p <- precomputed_curves[, which.min(dd)]
+
+## find best fit with sigma=1
+dd[parmat[, "sigma2"] != 1] <- Inf
+mu_fit <- parmat[which.min(dd), 1]
+acs_hat_1p <- precomputed_curves[, which.min(dd)]
+
+plot(1:250, acs, type = "l", ylim = c(0, 1))
+lines(acs_hat_2p, col = "blue")
+lines(acs_hat_1p, col = "red")
+abline(v = k_sub)
+legend(150, 0.5, col = c("black", "red", "blue"), lwd = 1, 
+       legend = c("empirical", "1par", "2par"))
