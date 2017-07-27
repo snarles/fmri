9a805263d35e8b54d0ecf54a27291721c252a2c7
Charles Zheng <snarles@stanford.edu>
2017-07-27 13:36:14 -0400

    single-run plot

---
+++ approximation/mc_gaussian_sim2_oneplot.R
@@ -0,0 +1,82 @@
+library(parallel)
+library(abind)
+
+mcc <- 40
+
+source("approximation/gaussian_identity_finsam.R")
+source("extrapolation/ku_source.R")
+
+p <- 10
+
+
+
+p <- 10
+sigma2 <- 0.3
+sigma2_tr <- sigma2 ## equivalent to 1 nn
+K <- 2000
+ksub <- 500
+
+load("approximation/mcgs2f.rda", verbose = TRUE) ## get the true_acces
+matplot(t(all_accs), type = "l")
+ses <- apply(all_accs, 2, sd)/sqrt(nrow(all_accs))
+plot(1:K, true_accs, type = "l", ylim = c(0, 1))
+lines(1:K, true_accs + ses, col = "red")
+lines(1:K, true_accs - ses, col = "red")
+
+
+nsplines <- 100
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- rev(1 - knts[-c(1, nsplines + 2)]^2)
+MM_2_sq <- spline1_moments(knts, 1:K)
+
+nsplines <- 100
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- knts[-c(1, nsplines + 2)]
+MM_2 <- spline1_moments(knts, 1:K)
+
+nsplines <- 1000
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- knts[-c(1, nsplines + 2)]
+MM_3 <- spline1_moments(knts, 1:K)
+
+nsplines <- 1000
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- rev(1 - knts[-c(1, nsplines + 2)]^2)
+MM_3_sq <- spline1_moments(knts, 1:K)
+
+nsplines <- 10000
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- knts[-c(1, nsplines + 2)]
+MM_4 <- spline1_moments(knts, 1:K)
+
+nsplines <- 10000
+knts <- seq(0, 1, length.out = nsplines + 2)
+knts <- rev(1 - knts[-c(1, nsplines + 2)]^2)
+MM_4_sq <- spline1_moments(knts, 1:K)
+
+basis_vecs <- list(mm2 = MM_2, mm2sq =MM_2_sq, mm3 = MM_3, mm3sq = MM_3_sq, mm4 = MM_4, mm4sq = MM_4_sq)
+
+t1 <- proc.time()
+repno <- 1
+set.seed(repno)
+mus <- randn(K, p)
+noise1 <- randn(K, p)
+noise2 <- randn(K, p)
+ys <- mus + sqrt(sigma2) * noise1
+mu_hats <- mus + sqrt(sigma2_tr) * noise2
+pmat <- -pdist2(mu_hats, ys)
+accs <- 1 - resample_misclassification(pmat, 1:K, 1:K)
+pmat_sub <- pmat[1:ksub, 1:ksub]
+accs_sub <- 1 - resample_misclassification(pmat_sub, 1:ksub, 1:ksub)
+preds <- matrix(0, length(basis_vecs), K)
+for (ind in 1:length(basis_vecs)) {
+  MM <- basis_vecs[[ind]]
+  bt <- nnls::nnls(MM[1:ksub, ], 1- accs_sub)
+  preds[ind, ] <- 1 - (MM %*% bt$x)
+}
+proc.time() - t1
+
+matplot(1:K, t(preds), type = "l")
+lines(1:K, true_accs, lwd = 3)
+
+save(true_accs, preds, file = "approximation/mcgs2_oneplot.rda")
