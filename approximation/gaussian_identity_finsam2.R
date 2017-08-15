20727fb4ca9940793d4751bcbbff14579a99fb46
Charles Zheng <snarles@stanford.edu>
2017-08-15 17:39:05 -0400

    rcpp version for finding ATA_k

--- approximation/gaussian_identity_finsam2.R
+++ approximation/gaussian_identity_finsam2.R
@@ -7,11 +7,11 @@ mcc <- 4
 
 ## query points x_i which are within distance r of x
 
-sigma2 <- 0.2
-sigma2_tr <- 0.1
+sigma2 <- 0.05
+sigma2_tr <- 0.05
 
 p <- 10
-n <- 4e3
+n <- 1e5
 mus <- randn(n, p)
 
 muhs <- mus + sqrt(sigma2_tr) * randn(n, p)
@@ -22,13 +22,16 @@ t1 <- proc.time()
 counts <- countDistEx(muhs, ys, rSqs)
 (cpp_time <- proc.time() - t1)
 
+1 - mean(counts != 0) ## accuracy
+
+## takes 290s for 1e5
 
 ## naive way
-t1 <- proc.time()
-dd <- pdist2(muhs, ys)^2
-counts0 <- sapply(1:n, function(i) sum(dd[-i, i] < rSqs[i]))
-(naive_time <- proc.time() -t1)
-
-sum(counts != counts0)
-1 - mean(counts != 0) ## accuracy
-rbind(cpp_time, naive_time)
+# t1 <- proc.time()
+# dd <- pdist2(muhs, ys)^2
+# counts0 <- sapply(1:n, function(i) sum(dd[-i, i] < rSqs[i]))
+# (naive_time <- proc.time() -t1)
+# 
+# sum(counts != counts0)
+# 1 - mean(counts != 0) ## accuracy
+# rbind(cpp_time, naive_time)
