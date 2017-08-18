81fd95b8ed2cc31227af37f68c3d76d3dafb671b
Charles Yang Zheng <zhengchy@felix.nimh.nih.gov>
2017-08-18 15:56:44 -0400

    it works.. so far no diff between r250 and r1000

--- approximation/mc_gaussian_sim2_fpo_knots_and_rows.R
+++ approximation/mc_gaussian_sim2_fpo_knots_and_rows.R
@@ -1,5 +1,5 @@
 library(parallel)
-mcc <- 20
+mcc <- 100
 
 source("approximation/gaussian_identity_finsam.R")
 source("extrapolation/ku_source.R")
@@ -9,7 +9,7 @@ sigma2 <- 0.3
 sigma2_tr <- sigma2 ## equivalent to 1 nn
 K <- 2000
 ksub <- 1000
-mc.reps <- 180
+mc.reps <- 100
 
 # mus <- randn(K, p)
 # ys <- mus + sqrt(sigma2) * randn(K, p)
@@ -26,8 +26,8 @@ combmat <- cbind(nsplines = rep(nsplines, each = length(nrows)),
 basis_vecs <- list()
 
 kseq <- function(nr) {
-  interv <- floor(K/nr)
-  seq(interv, K, by = interv)
+  interv <- floor(ksub/nr)
+  seq(interv, ksub, by = interv)
 }
 
 for (ii in 1:nrow(combmat)) {
@@ -41,10 +41,6 @@ for (ii in 1:nrow(combmat)) {
   basis_vecs[[nm]] <- list(Xmat = Xmat, Xpred = Xpred, kseq = kseq(nr))
 }
 
-all_final_preds <- array(0, dim = c(mc.reps, length(basis_vecs)))
-all_accs <- matrix(0, mc.reps, K)
-
-t1 <- proc.time()
 
 subfun <- function (repno) {
   set.seed(repno)
@@ -53,7 +49,7 @@ subfun <- function (repno) {
   mu_hats <- mus + sqrt(sigma2_tr) * randn(K, p)
   pmat <- -pdist2(mu_hats, ys)
   accs <- 1 - resample_misclassification(pmat, 1:K, 1:K)
-  all_accs[repno, ] <- accs  
+  #all_accs[repno, ] <- accs  
   pmat_sub <- pmat[1:ksub, 1:ksub]
   accs_sub <- 1 - resample_misclassification(pmat_sub, 1:ksub, 1:ksub)
   preds <- numeric(length(basis_vecs))
@@ -64,39 +60,30 @@ subfun <- function (repno) {
     bt <- nnls::nnls(Xmat, 1- accs_sub[ks])
     preds[ind] <- 1 - (Xpred %*% bt$x)
   }
-  preds
+  list(preds = preds, accs = accs)
 }  
 
 ## !!TBC!!
 
 t1 <- proc.time()
-preds <- mclapply(1:mc.reps, subfun, mc.cores = mcc)
-preds2 <- do.call(rbind, preds)
-all_final_preds[repno, , ] <- preds2
-proc.time() - t1
-# "12.5 per hour"
+res <- mclapply(1:mc.reps, subfun, mc.cores = mcc)
+(runtime <- proc.time() - t1)
 
-true_accs <- colMeans(all_accs)
-errs <- (all_final_preds - true_accs[K])^2
+# 46s
 
-final_pred_err_mat <- apply(errs, c(2, 3), mean)
-#final_pred_err_mat <- apply(errs, c(2, 3), median)
-#plot(all_final_preds[,40,5])
-#final_pred_err_mat <- apply(errs[sample(mc.reps, replace = TRUE), , ], c(2, 3), mean)
-#load("approximation/mcgs2f.rda")
-pdf("approximation/fig_mcgs2f.pdf", width = 5, height = 4)
-source("approximation/mcgs2_colscheme.R")
-matplot(ksubs, sqrt(final_pred_err_mat), 
-        type = "l", ylab = "RMSE", xlab = expression(k[1]), xlim = c(250, 2000),
-        ylim = c(0, 0.3), col = cols, lty = ltys, lwd = 3)
-# matplot(ksubs, sqrt(final_pred_err_mat), 
-#         type = "l", ylab = "RMSE", xlab = expression(k[1]),
-#         col = cols, lty = ltys, lwd = 3, ylim = c(0, 0.5))
+resa <- lapply(res, `[[`, "accs")
+true_accs <- colMeans(do.call(rbind, resa))
+plot(true_accs, type="l", ylim = c(0,1))
 
-legend(1570, 0.32, legend = nms, col = cols, lty = ltys, lwd = 3)
-dev.off()
+(facc <- true_accs[K])
+all_final_preds <- do.call(rbind, lapply(res, `[[`, "preds"))
+colnames(all_final_preds) <- names(basis_vecs)
+resids <- all_final_preds - facc
 
-#save(all_accs, errs, true_accs, final_pred_err_mat, file = "approximation/mcgs2f.rda")
-
-
-
+boxplot(resids^2)
+rmses <- sqrt(colMeans(resids^2))
+dispmat <- matrix(rmses, length(nrows), length(nsplines))
+rownames(dispmat) <- paste0("r", nrows)
+colnames(dispmat) <- paste0("s", nsplines)
+dispmat
+#matrix(names(rmses), length(nrows), length(nsplines))
