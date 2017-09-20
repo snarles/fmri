7b32f95e43a18b400e89d074494b6daf1963a39c
Charles Yang Zheng <zhengchy@felix.nimh.nih.gov>
2017-09-20 13:28:26 -0400

    batch version

---
+++ approximation/mc_gaussian_sim5batch.R
@@ -0,0 +1,88 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly = TRUE)
+
+## Varying sigma
+## Simplified n. rows, and added kde method
+
+source("approximation/gaussian_identity_finsam.R")
+source("approximation/gaussian_identity_finsam2.R")
+source("extrapolation/ku_source.R")
+source("extrapolation/kay_method.R")
+
+mcc <- 50
+
+p <- 10
+sigma2_seq <- 0.02 * 1:20
+# sigma2 <- 0.07
+# sigma2_tr <- sigma2 ## equivalent to 1 nn
+K <- 10000 ## multiple of 1000
+Ktarg <- c(1000, 2000, 5000, 10000)
+ksub <- 500 ## multiple of 250
+mc.reps <- 800
+sigma2s <- rep(sigma2_seq, floor(mc.reps/length(sigma2_seq)))
+
+# mus <- randn(K, p)
+# ys <- mus + sqrt(sigma2) * randn(K, p)
+# mu_hats <- mus + sqrt(sigma2_tr) * randn(K, p)
+# pmat <- -pdist2(mu_hats, ys)
+# accs <- 1 - resample_misclassification(pmat, 1:K, 1:K)
+# plot(accs, type = "l")
+
+kseq <- function(nr, ksub) {
+  interv <- floor(ksub/nr)
+  seq(interv, ksub, by = interv)
+}
+
+nsplines <- c(100, 200, 400)
+nrow <- 125
+#kz <- kseq(1000, K)
+kref <- kseq(nrow, ksub)
+kde_bdwids <- list("bcv", "ucv", 0.1, 0.2, 0.3, 0.4)
+
+basis_vecs <- list()
+
+for (ii in 1:length(nsplines)) {
+  ns <- nsplines[ii]
+  nm <- paste0("spline",ns)
+  knts <- seq(0, 1, length.out = ns + 2)
+  knts <- rev(1 - knts[-c(1, ns + 2)]^2)  
+  Xmat <- spline1_moments(knts, kref)
+  Xpred <- spline1_moments(knts, Ktarg)
+  basis_vecs[[nm]] <- list(Xmat = Xmat, Xpred = Xpred)
+}
+
+(kde.names <- sapply(kde_bdwids, function(v) paste0("kde_", v)))
+column_names <- c(names(basis_vecs), kde.names)
+
+repno <- 10
+subfun <- function (repno) {
+  set.seed(repno)
+  sigma2 <- sigma2s[repno]
+  sigma2_tr <- sigma2s[repno]
+  mus <- randn(K, p)
+  ys <- mus + sqrt(sigma2) * randn(K, p)
+  mu_hats <- mus + sqrt(sigma2_tr) * randn(K, p)
+  pmat_sub <- -pdist2(ys[1:ksub, ], mu_hats[1:ksub, ])
+  rSqs <- rowSums((ys - mu_hats)^2)
+  counts <- countDistEx(mu_hats, ys, rSqs)
+  accs <- count_acc(counts, Ktarg)    
+  counts_sub <- countDistEx(mu_hats[1:ksub,], ys[1:ksub,], rSqs[1:ksub])
+  accs_sub <- count_acc(counts_sub, kref)
+  preds <- matrix(NA, length(column_names), length(Ktarg))
+  rownames(preds) <- column_names
+  for (ind in 1:length(column_names)) {
+    if (ind <= length(basis_vecs)) {
+      Xmat <- basis_vecs[[ind]]$Xmat
+      Xpred <- basis_vecs[[ind]]$Xpred
+      bt <- nnls::nnls(Xmat, 1- accs_sub)
+      preds[ind, ] <- 1 - (Xpred %*% bt$x)
+    } else {
+      bw <- kde_bdwids[[ind - length(basis_vecs)]]
+      preds[ind, ] <- kernel_extrap(pmat_sub, Ktarg, bw = bw)
+    }
+  }
+  list(preds = preds, accs = accs)
+}  
+
+res <- sapply(as.numeric(args[1]):as.numeric(args[2]), subfun)
+save(res, paste0(args[3], "_", args[1], "_", args[2]))
