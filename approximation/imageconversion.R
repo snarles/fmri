e47edd937f68263c45982ee0fb59a75f02f2f463
Charles Zheng <snarles@stanford.edu>
2017-11-14 17:08:01 -0500

    edits

--- approximation/imageconversion.R
+++ approximation/imageconversion.R
@@ -1,6 +1,8 @@
-ff <- scan("approximation/temp.txt", character())
-cmds <- sapply(ff, function(s)
-  paste0("convert -units PixelsPerInch ",s," -density 300 ",s,"\n"))
-sink("approximation/temp2.txt")
+ff <- scan("info_theory_paper/extrapolation_simple/temp.txt", character())
+ff2 <- scan("info_theory_paper/extrapolation_simple/tempp.txt", character())
+
+cmds <- sapply(1:length(ff), function(i)
+  paste0("convert -units PixelsPerInch ",ff[i]," -density 72 ",ff2[i],"\n"))
+sink("info_theory_paper/extrapolation_simple/temp2.txt")
 for (c in cmds) cat(c)
 sink()
