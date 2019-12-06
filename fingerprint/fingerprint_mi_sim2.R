2b54db42cabe5d80aae1620affb67427f08e2523
Charles Zheng <snarles@stanford.edu>
2019-12-05 19:02:08 -0500

    full mi est pipeline

--- fingerprint/fingerprint_mi_sim2.R
+++ fingerprint/fingerprint_mi_sim2.R
@@ -8,10 +8,10 @@ library(lineId) ## use devtools::install('lineId')
 source('fingerprint/mi_est_pipeline.R')
 
 
-K <- 30
+K <- 50
 d <- 18
-p <- 100
-sigma2 <- 0.3
+p <- 35
+sigma2 <- 0.7
 
 xs <- randn(K, d)
 amat <- randn(d, p)
@@ -30,15 +30,21 @@ resample_misclassification(pmat, 1:K)
 K <- dim(pmat)[1]
 empirical_acc <- 1- resample_misclassification(pmat, 1:K)
 
-mi_grid <- 3 * (1:300)/200
+mi_grid <- 3 * (1:600)/100
 pi_ests <- piK(sqrt(2 * mi_grid), K)
-mi_est <- max(mi_grid[(1-pi_ests) < empirical_acc ])
-mi_est
+mi_xx_est <- max(mi_grid[(1-pi_ests) < empirical_acc ])
+mi_xx_est
 
 component_mis <- sapply(1:p, function(i) mutinfo(ys[, i], ys2[, i]))
 sum(component_mis)
 
-ratio <- sum(component_mis)/mi_est
+ratio <- sum(component_mis)/mi_xx_est
 est_d <- p/ratio
 est_d
 
+# mi per dimension
+mi_per_dim <- mi_xx_est/est_d
+# invert to rho since -1/2 * log(1 - rho^4) = I(X;X')
+rho_est <- (1 - exp(-2 * mi_per_dim)) ^ (.25)
+mi_xy_est <- -est_d/2 * log(1 - rho_est^2)
+c(xx=mi_xx_est, est=mi_xy_est, true=mi_true)
